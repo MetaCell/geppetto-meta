17056005de3e6eebef864fd7abaff9a3e3c83cb0
Dario Del Piano <dario@metacell.us>
2019-07-19 04:21:42 +0200

    Style factored out, items aligned, icons implemented

--- js/components/interface/menu/MenuSingleItem.js
+++ js/components/interface/menu/MenuSingleItem.js
@@ -1,41 +1,6 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import MenuPopper from './MenuPopper';
 import MenuItem from '@material-ui/core/MenuItem';
-import { withStyles } from '@material-ui/core/styles';
-import ArrowRightIcon from "@material-ui/icons/KeyboardArrowRight";
-
-const styles = {
-  root1: {
-    backgroundColor: '#44414112',
-    "&:hover": {
-      background: "#11bffe",
-      backgroundColor: "#11bffe",
-      color: '#ffffff'
-    },
-    borderRadius: 0,
-    color: '#ffffff',
-    fontSize: '14px',
-    fontFamily: 'Khand, sans-serif',
-    paddingTop: 0,
-    paddingBottom: 0,
-  },
-  root2: {
-    background: "#11bffe",
-    backgroundColor: "#11bffe",
-    "&:hover": {
-      background: "#11bffe",
-      backgroundColor: "#11bffe",
-      color: '#ffffff'
-    },
-    borderRadius: 0,
-    color: '#ffffff',
-    fontSize: '14px',
-    fontFamily: 'Khand, sans-serif',
-    paddingTop: 0,
-    paddingBottom: 0,
-  }
-};
 
 class MenuSingleItem extends React.Component {
   constructor (props) {
@@ -44,7 +9,7 @@ class MenuSingleItem extends React.Component {
     this.state = {
       anchorEl: null,
       subMenuOpened: false,
-      sectionOpened: undefined
+      sectionOpened: undefined,
     }
 
     this.handleClick = this.handleClick.bind(this);
@@ -82,25 +47,27 @@ class MenuSingleItem extends React.Component {
             menuHandler={this.props.menuHandler}
             menuHandlerDirect={this.props.menuHandlerDirect}
             awayClickHandler={this.props.awayClickHandler}
+            drawersStyle={this.props.drawersStyle}
+            labelsStyle={this.props.labelsStyle}
           />);
-          MenuItemClass = { root: classes.root2 };
+          MenuItemClass = this.props.labelsStyle.hover;
         } else {
           menuToRender = undefined;
-          MenuItemClass = { root: classes.root1 };
+          MenuItemClass = this.props.labelsStyle.standard;
         }
         return (
           <MenuItem
             id={index}
             key={index}
             onClick={e => {
-              this.handleClick(e, item.action) 
+              this.handleClick(e, item.action)
             }}
             onMouseOver={e => {
-              this.handleMouseOver(e, index) 
+              this.handleMouseOver(e, index)
             }}
             ContainerProps={{ menuHandlerDirect: this.props.menuHandlerDirect }}
-            classes={MenuItemClass}>
-            {item.icon !== "" ? <i className={item.icon} style={{ marginRight: '10px' }}></i> : undefined}
+            style={MenuItemClass}>
+            {item.icon !== "" ? <span style={{ display: "inline-block", width: "25px" }}><i className={item.icon}></i></span> : undefined}
             {item.label}
             <i className="fa fa-chevron-right" style={{
               marginLeft: '10px',
@@ -122,25 +89,27 @@ class MenuSingleItem extends React.Component {
             menuHandler={this.props.menuHandler}
             menuHandlerDirect={this.props.menuHandlerDirect}
             awayClickHandler={this.props.awayClickHandler}
+            drawersStyle={this.props.drawersStyle}
+            labelsStyle={this.props.labelsStyle}
           />);
-          MenuItemClass = { root: classes.root2 };
+          MenuItemClass = this.props.labelsStyle.hover;
         } else {
           menuToRender = undefined;
-          MenuItemClass = { root: classes.root1 };
+          MenuItemClass = this.props.labelsStyle.standard;
         }
         return (
           <MenuItem
             id={index}
             key={index}
             onClick={e => {
-              this.handleClick(e, item.action) 
+              this.handleClick(e, item.action)
             }}
             onMouseOver={e => {
-              this.handleMouseOver(e, index) 
+              this.handleMouseOver(e, index)
             }}
             ContainerProps={{ menuHandlerDirect: this.props.menuHandlerDirect }}
-            classes={MenuItemClass}>
-            {item.icon !== "" ? <i className={item.icon} style={{ marginRight: '10px' }}></i> : undefined}
+            style={MenuItemClass}>
+            {item.icon !== "" ? <span style={{ display: "inline-block", width: "25px" }}><i className={item.icon}></i></span> : undefined}
             {item.label}
             <i className="fa fa-chevron-right" style={{
               marginLeft: '10px',
@@ -153,20 +122,29 @@ class MenuSingleItem extends React.Component {
           </MenuItem>
         );
       } else {
+        if ((anchorEl !== null) && (anchorEl !== undefined) && (index === Number(this.state.sectionOpened))) {
+          MenuItemClass = this.props.labelsStyle.hover;
+        } else {
+          MenuItemClass = this.props.labelsStyle.standard;
+        }
         return (
           <MenuItem
             key={index}
             onClick={e => {
-              this.handleClick(e, item.action) 
+              this.handleClick(e, item.action)
             }}
             onMouseOver={e => {
-              this.handleMouseOver(e, index) 
+              this.handleMouseOver(e, index)
             }}
             ContainerProps={{ menuHandlerDirect: this.props.menuHandlerDirect }}
             menuaction={item.action}
-            classes={{ root: classes.root1, }}
+            style={MenuItemClass}
           >
-            {item.icon !== "" ? <i className={item.icon} style={{ marginRight: '10px' }}></i> : undefined}
+            {item.icon !== ""
+              ? <span style={{ display: "inline-block", width: "25px" }}>
+                <i className={item.icon}></i>
+              </span>
+              : undefined}
             {item.label}
           </MenuItem>
         );
@@ -181,6 +159,4 @@ class MenuSingleItem extends React.Component {
   }
 }
 
-MenuSingleItem.propTypes = { classes: PropTypes.object.isRequired, };
-
-export default withStyles(styles)(MenuSingleItem);
+export default MenuSingleItem;
