387a7db899ef388d802fb9f91cd4f5e83de87a4a
jrmartin <yeshuarmartinez@gmail.com>
2019-09-16 10:13:34 -0700

    adding off before on to avoid duplicate listener on geppetto events

--- js/components/interface/experimentsTable/ExperimentsTable.js
+++ js/components/interface/experimentsTable/ExperimentsTable.js
@@ -519,20 +519,20 @@ define(function (require) {
         }
       });
 
-      GEPPETTO.on(GEPPETTO.Events.Project_loaded, function () {
+      GEPPETTO.off(GEPPETTO.Events.Project_loaded).on(GEPPETTO.Events.Project_loaded, function () {
         self.populate();
         self.updateStatus();
       });
 
-      GEPPETTO.on(GEPPETTO.Events.Project_persisted, function () {
+      GEPPETTO.off(GEPPETTO.Events.Project_persisted).on(GEPPETTO.Events.Project_persisted, function () {
         self.refresh();
       });
             
-      GEPPETTO.on(GEPPETTO.Events.Experiment_status_check, function () {
+      GEPPETTO.off(GEPPETTO.Events.Experiment_status_check).on(GEPPETTO.Events.Experiment_status_check, function () {
         self.updateExperimentsTableStatus();
       });
 
-      GEPPETTO.on(GEPPETTO.Events.Experiment_loaded, function () {
+      GEPPETTO.off(GEPPETTO.Events.Experiment_loaded).on(GEPPETTO.Events.Experiment_loaded, function () {
         self.updateExperimentStatus();
       });
 
@@ -541,10 +541,11 @@ define(function (require) {
       });
 
 
-      GEPPETTO.on(GEPPETTO.Events.Experiment_renamed, function (experiment) {
+      GEPPETTO.off(GEPPETTO.Events.Experiment_renamed).on(GEPPETTO.Events.Experiment_renamed, function (experiment) {
         self.refresh();
       });
 
+      //Avoid adding the same listener twice by first calling off()
       GEPPETTO.off(GEPPETTO.Events.Experiment_deleted).on(GEPPETTO.Events.Experiment_deleted, function (experiment) {
         self.deleteExperiment(experiment);
         if (!GEPPETTO.ExperimentsController.suppressDeleteExperimentConfirmation) {
