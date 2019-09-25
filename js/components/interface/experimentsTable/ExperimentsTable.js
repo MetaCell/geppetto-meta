7cfe77ba8500fe9162cf7aa0c8bf6568b9eff482
jrmartin <yeshuarmartinez@gmail.com>
2019-09-25 09:59:14 -0700

    unattach GEPPETTO.EVENTS from listeners when component gets unmounted

--- js/components/interface/experimentsTable/ExperimentsTable.js
+++ js/components/interface/experimentsTable/ExperimentsTable.js
@@ -576,6 +576,18 @@ define(function (require) {
          
       $("#experimentsButton").show();
     },
+    
+    componentWillUnmount () {
+      // stop listening to events on unmount
+      GEPPETTO.off(GEPPETTO.Events.Experiment_status_check);
+      GEPPETTO.off(GEPPETTO.Events.Experiment_loaded);
+      GEPPETTO.off(GEPPETTO.Events.Experiment_renamed);
+      GEPPETTO.off(GEPPETTO.Events.Experiment_created);
+      GEPPETTO.off(GEPPETTO.Events.Experiment_deleted);
+      GEPPETTO.off(GEPPETTO.Events.Project_loaded);
+      GEPPETTO.off(GEPPETTO.Events.Project_persisted);
+      console.log("exp will un mount")
+    },
         
     refresh: function (){
       this.forceUpdate();
