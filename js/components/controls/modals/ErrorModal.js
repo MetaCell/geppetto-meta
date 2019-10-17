66fe0f87a9ae538284b935f04e11b3cd5a4c3b6e
jrmartin <yeshuarmartinez@gmail.com>
2019-10-17 16:22:03 -0700

    Uses properties in GeppettoConfiguration.json (if present)

--- js/components/controls/modals/ErrorModal.js
+++ js/components/controls/modals/ErrorModal.js
@@ -21,15 +21,44 @@ define(function (require) {
         text: '',
         code: '',
         source: '',
-        exception: '' 
+        exception: '',
+        githubButton : {
+          enabled : false,
+          url : "https://github.com/openworm/org.geppetto/issues/new"
+        },
+        twitterButton : {
+          enabled : false,
+          url : "http://geppetto.org",
+          message : "Whoops, I broke Geppetto! @geppettoengine help!"
+        }
       }
+    },
+    
+    getProperty: function (property) {
+      return property.split(".").reduce(function(o, x) {
+        return (typeof o == "undefined" || o === null) ? o : o[x];
+      }, GEPPETTO_CONFIGURATION);
     },
         
     shareTwitter: function () {
-      GEPPETTO.Share.twitter('http://geppetto.org','Whoops, I broke Geppetto! @geppettoengine help!');
+      let urlProperty = this.getProperty("properties.errorDialog.twitterButton.url");
+      let url = ( urlProperty != undefined ? urlProperty : this.props.twitterButton.url)
+      let messageProperty = this.getProperty("properties.errorDialog.twitterButton.message");
+      let message = ( messageProperty != undefined ? messageProperty : this.props.twitterButton.message)
+      window.open('http://twitter.com/share?url=' + encodeURIComponent(url) + '&text='
+        + encodeURIComponent(message), '', 'left=0,top=0,width=550,height=450,personalbar=0,toolbar=0,scrollbars=0,resizable=0');
     },
         
     render: function (){
+      let twiButProp = (this.getProperty("properties.errorDialog.twitterButton.enabled") == undefined 
+        ? this.props.twitterButton.enabled : this.getProperty("properties.errorDialog.twitterButton.enabled"));
+      let twitterButtonVisible = ( twiButProp ? null : { display: "none" })
+      let gitButProp = (this.getProperty("properties.errorDialog.githubButton.enabled") == undefined 
+        ? this.props.githubButton.enabled : this.getProperty("properties.errorDialog.githubButton.enabled"));
+      let githubButtonVisible = ( gitButProp ? null : { display: "none" })
+      let confGitURL = this.getProperty("properties.errorDialog.githubButton.url");
+      let githubButtonURL = (confGitURL != undefined ? confGitURL : this.props.githubButton.url)
+      let githubMessage = this.getProperty("properties.errorDialog.message")
       return (
         <div className="modal fade" id="errormodal">
           <div className="modal-dialog">
@@ -39,7 +68,7 @@ define(function (require) {
               </div>
               <div className="modal-body">
                 <p id="errormodal-text">
-                  {this.props.message}
+                  {githubMessage != undefined ? githubMessage : this.props.message}
                 </p>
                 <div className="panel panel-default">
                   <div className="panel-heading">
@@ -56,10 +85,10 @@ define(function (require) {
                 </div>
               </div>
               <div className="modal-footer" id="errormodal-footer">
-                <button className="btn" onClick={this.shareTwitter} aria-hidden="true">
+                <button className="btn" onClick={this.shareTwitter} aria-hidden="true" style={twitterButtonVisible}>
                   <i className="fa fa-twitter"></i> Shame on you
                 </button>
-                <a className="btn" href="https://github.com/openworm/org.geppetto/issues/new" target="_blank" aria-hidden="true">
+                <a className="btn" href={githubButtonURL} target="_blank" aria-hidden="true" style={githubButtonVisible}>
                   <i className="fa fa-bug"></i> Open issue
                 </a>
                 <button id="errormodal-btn" className="btn" data-dismiss="modal" aria-hidden="true">Close</button>
