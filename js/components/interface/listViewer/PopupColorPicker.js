57ef8925e85a77c44e5ea00f1d6fc255d696ae8b
rodriguez-facundo <bruseghini.f@gmail.com>
2019-11-28 09:56:52 -0300

    #126 Custom collide radius and close color picker on click away for listViewer

--- js/components/interface/listViewer/PopupColorPicker.js
+++ js/components/interface/listViewer/PopupColorPicker.js
@@ -1,6 +1,21 @@
 import React from 'react';
+import Popover from '@material-ui/core/Popover';
 import { SketchPicker, CompactPicker, SwatchesPicker } from 'react-color';
 import BaseIconComponent from './BaseIconComponent';
+
+const styles = {
+  container: { position: 'relative' },
+  popover: {
+    anchorOrigin:{
+      vertical: 'bottom',
+      horizontal: 'center',
+    },
+    transformOrigin:{
+      vertical: 'top',
+      horizontal: 'center',
+    }
+  },
+};
 
 export default class PopupColorPicker extends React.Component {
   
@@ -8,48 +23,59 @@ export default class PopupColorPicker extends React.Component {
   constructor (props){
     super(props);
     this.state = {
-      displayColorPicker: false,
+      anchorEl: null,
       color: props.color ? props.color : '#FFFFFF',
     };
     this.Picker = this.props.picker ? this.props.picker : CompactPicker;
   }
 
-  handleClick () {
-    return this.setState({ displayColorPicker: !this.state.displayColorPicker });
+  handleClick (currentTarget) {
+    this.setState({ anchorEl: currentTarget });
   }
 
   handleClose () {
-    this.setState({ displayColorPicker: false });
+    this.setState({ anchorEl: null });
   }
 
   handleChange (color) {
-    this.setState({ color: color.hex, displayColorPicker: false });
+    this.setState({ color: color.hex, anchorEl: null });
     this.props.action(color.hex);
   }
+  
 
   render () {
-
-    const styles = {
-      container: { position: 'relative' },
-      popover: {
-        position: 'absolute',
-        zIndex: '2',
-        right: 0
-      },
-        
-    };
-
+    const { color, anchorEl } = this.state
+    const open = Boolean(anchorEl);
+    var container
+    
+    if (open) {
+      const colorPicker = React.createElement(this.Picker, { 
+        color, 
+        onChange: color => this.handleChange(color) 
+      })
+      container = (
+        <Popover 
+          open={open} 
+          anchorEl={anchorEl} 
+          onClose={() => this.handleClose()}
+          anchorOrigin={styles.popover.anchorOrigin}
+          transformOrigin={styles.popover.transformOrigin}
+        >
+          {colorPicker}
+        </Popover>
+      )
+    } else {
+      container = null
+    }
+    
     return (
       <div style={ styles.container } >
-        <BaseIconComponent action={ () => this.handleClick() } color={ this.state.color } icon={this.props.icon ? this.props.icon : "tint"} />
-
-        { this.state.displayColorPicker ? <div style={ styles.popover }>
-          <div style={ styles.cover } onClick={ () => this.handleClose() }/>
-          {
-            React.createElement(this.Picker, { color: this.state.color, onChange: color => this.handleChange(color) })
-          }
-        </div> : null }
-
+        <BaseIconComponent 
+          action={e => this.handleClick(e.currentTarget)} 
+          color={ color } 
+          icon={this.props.icon ? this.props.icon : "tint"}
+        />
+        { container }
       </div>
     )
   }
