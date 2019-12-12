d99361429575213d2ae18c21aef7357c97839920
Afonso Pinto <afonsobspinto@gmail.com>
2019-12-12 11:01:36 +0100

    Add workaround to get data

--- js/components/interface/connectivity/ConnectivityComponent.js
+++ js/components/interface/connectivity/ConnectivityComponent.js
@@ -13,13 +13,17 @@ export default class ConnectivityComponent extends AbstractComponent {
   }
 
   componentDidMount () {
-    this.connectivityContainer = this.selectElement(this.id);
+    this.connectivityContainer = this.selectElement(this.props.id);
     this.innerHeight = this.getInnerHeight(this.connectivityContainer) - this.widgetMargin;
     this.innerWidth = this.getInnerWidth(this.connectivityContainer) - this.widgetMargin;
-    this.drawLayout();
   }
 
-  drawLayout () {
+  componentWillUpdate (prevProps, prevState, snapshot) {
+    console.log("HERE");
+    console.log(Window.workaround)
+  }
+
+  createLayout () {
     this.cleanCanvas();
     this.svg = d3.select(this.props.id)
       .append("svg")
@@ -41,9 +45,11 @@ export default class ConnectivityComponent extends AbstractComponent {
   getInnerHeight (el){
     return parseFloat(getComputedStyle(el, null).height.replace("px", ""))
   }
+
   selectElement (cssSelector){
     return document.getElementById(cssSelector);
   }
+
   removeElement (cssSelector){
     const container = document.querySelectorAll(cssSelector);
 
@@ -53,6 +59,24 @@ export default class ConnectivityComponent extends AbstractComponent {
         element.parentNode.removeChild(element);
       }
     }
+  }
+
+  //
+  // createElement(type, options){
+  //   let el = document.createElement(type)
+  //   for
+  // }
+  //
+  // append(dest, element){
+  //   dest.appendChild(element);
+  // }
+  //
+  // appendTo (dest, element){
+  //
+  // }
+
+  extend (obj1, obj2){
+    Object.assign(obj1, obj2);
   }
 
   getLayout (svgCanvas) {
@@ -69,12 +93,114 @@ export default class ConnectivityComponent extends AbstractComponent {
     }
   }
 
+  setData (data){
+    this.setOptions(data.options);
+    this.dataset = {};
+    this.mapping = {};
+    this.mappingSize = 0;
+    this.dataset["root"] = data.root;
+    this.setNodeColormap(data.nodeColormap);
+    if (this.createDataFromConnections()){
+      this.createLayout();
+    }
+  }
+
+  setOptions (options){
+    function strToFunc (body){
+      return new Function('x', 'return ' + body + ';');
+    }
+    if (options != null) {
+      if (typeof options.linkType === 'string') {
+        options.linkType = strToFunc(options.linkType);
+      }
+      if (typeof options.nodeType === 'string') {
+        options.nodeType = strToFunc(options.nodeType);
+      }
+      if (typeof options.linkWeight === 'string') {
+        options.linkWeight = strToFunc(options.linkWeight);
+      }
+      if (typeof options.colorMapFunction === 'string') {
+        options.colorMapFunction = strToFunc(options.colorMapFunction);
+      }
+      if (typeof options.library === 'string') {
+        options.library = eval(options.library);
+      }
+      this.extend(this.options, options);
+    }
+  }
+
+  setNodeColormap (nodeColormap) {
+    if (typeof nodeColormap !== 'undefined') {
+      this.nodeColormap = nodeColormap;
+    } else {
+      this.nodeColormap = this.defaultColorMapFunction();
+    }
+    return this.nodeColormap;
+  }
+
+  defaultColorMapFunction () {
+    const cells = this.dataset["root"].getChildren();
+    const domain = [];
+    const range = [];
+    for (let i = 0; i < cells.length; ++i) {
+      if (cells[i].getMetaType() === GEPPETTO.Resources.ARRAY_INSTANCE_NODE) {
+        domain.push(cells[i].getName());
+        range.push(cells[i].getColor());
+      }
+    }
+    // if everything is default color, use a d3 provided palette as range
+    if (range.filter(function (x) {
+      return x !== GEPPETTO.Resources.COLORS.DEFAULT;
+    }).length === 0) {
+      return d3.scaleOrdinal(d3.schemeCategory20).domain(domain);
+    } else {
+      return d3.scaleOrdinal(range).domain(domain);
+    }
+  }
+
+  createDataFromConnections () {
+    const connectionVariables = GEPPETTO.ModelFactory.getAllTypesOfType(this.options.library.connection)[0].getVariableReferences();
+    if (connectionVariables.length > 0) {
+      if (this.dataset["root"].getMetaType() === GEPPETTO.Resources.INSTANCE_NODE) {
+        const subInstances = this.dataset["root"].getChildren();
+        this.dataset["nodes"] = [];
+        this.dataset["links"] = [];
+        for (let k = 0; k < subInstances.length; k++) {
+          const subInstance = subInstances[k];
+          if (subInstance.getMetaType() === GEPPETTO.Resources.ARRAY_INSTANCE_NODE) {
+            const populationChildren = subInstance.getChildren();
+            for (let l = 0; l < populationChildren.length; l++) {
+              let populationChild = populationChildren[l];
+              this.createNode(populationChild.getId(), this.options.nodeType(populationChild));
+            }
+          }
+        }
+        for (let x = 0; x < connectionVariables.length; x++){
+          const connectionVariable = connectionVariables[x];
+          let source = connectionVariable.getA();
+          let target = connectionVariable.getB();
+          let sourceId = source.getElements()[source.getElements().length - 1].getPath();
+          let targetId = target.getElements()[source.getElements().length - 1].getPath();
+          this.createLink(sourceId, targetId, this.options.linkType.bind(this)(connectionVariable, this.linkCache), this.options.linkWeight(connectionVariable));
+        }
+      }
+      this.dataset.nodeTypes = _.uniq(_.pluck(this.dataset.nodes, 'type')).sort();
+      this.dataset.linkTypes = _.uniq(_.pluck(this.dataset.links, 'type')).sort();
+      return true;
+    }
+    return false;
+  }
+
   render () {
     const { id } = this.props;
     return (
       <div>
         <ConnectivityDeck/>
-        <div id={id}/>
+        <div id={id}>
+          <button onClick={() => this.setData(Window.workaround)}>
+            Activate Lasers
+          </button>
+        </div>
       </div>
 
     )
--- js/components/interface/connectivity/layouts/Matrix.js
+++ js/components/interface/connectivity/layouts/Matrix.js
@@ -34,194 +34,194 @@ export class Matrix {
 
     // todo: Continue from here
 
-    const matrix = [];
-    const nodes = this.connectivity.dataset.nodes;
-    var root = this.connectivity.dataset.root;
-    var n = nodes.length;
-
-    // Compute index per node.
-    nodes.forEach(function (node, i) {
-      node.pre_count = 0;
-      node.post_count = 0;
-      matrix[i] = d3.range(n).map(function (j) {
-        return { x: j, y: i, z: 0 };
-      });
-    });
-
-    // Convert links to matrix; count pre / post conns.
-    this.connectivity.dataset.links.forEach(function (link) {
-      /*
-       * TODO: think about zero weight lines
-       * matrix[link.source][link.target].z = link.weight ? link.type : 0;
-       */
-      matrix[link.source][link.target].z = link.type;
-      nodes[link.source].pre_count += 1;
-      nodes[link.target].post_count += 1;
-    });
-
-    /*
-     *Sorting matrix entries.
-     *TODO: runtime specified sorting criteria
-     */
-    var sortOptions = {
-      'id': 'By entity name',
-      'pre_count': 'By # pre',
-      'post_count': 'By # post'
-    };
-    //  Precompute the orders.
-    var orders = {
-      id: d3.range(n).sort(function (a, b) {
-        return d3.ascending(nodes[a].id, nodes[b].id);
-      }),
-      pre_count: d3.range(n).sort(function (a, b) {
-        return nodes[b].pre_count - nodes[a].pre_count;
-      }),
-      post_count: d3.range(n).sort(function (a, b) {
-        return nodes[b].post_count - nodes[a].post_count;
-      }),
-      // community: d3.range(n).sort(function(a, b) { return nodes[b].community - nodes[a].community; }),
-    };
-    // Default sort order.
-    x.domain(orders.id);
-
-    var rect = container
-      .append("rect")
-      .attr("class", "background")
-      .attr("width", matrixDim - margin.left - 30)
-      .attr("height", matrixDim - margin.top);
-
-    /*
-     * we store the 'conn' key in case we want to
-     * eg. conditionally colour the indicator if there
-     * are actually connections in that row/column
-     */
-    var pre = nodes.map(function (x,i) {
-      return {
-        id: x.id, conn: matrix[i].filter(function (d) {
-          return d.z;
-        }).length > 0
-      }
-    });
-    var matrixT = matrix[0].map(function (col, i) {
-      return matrix.map(function (row) {
-        return row[i];
-      })
-    });
-    var post = nodes.map(function (x,i) {
-      return {
-        id: x.id, conn: matrixT[i].filter(function (d) {
-          return d.z;
-        }).length > 0
-      }
-    });
-
-    var popNameFromId = function (id) {
-      return eval(GEPPETTO.ModelFactory.getAllPotentialInstancesEndingWith(id)[0]).getParent().getName();
-    };
-
-    var mouseoverCell = function (msg) {
-      d3.select(this.parentNode.appendChild(this)).transition().duration(100).
-        style('stroke-opacity', 1).style('stroke', 'white').style('stroke-width', 2);
-      d3.select("body").style('cursor', 'pointer');
-      return tooltip.transition().duration(100).text(msg);
-    }
-
-    var mouseoutCell = function () {
-      d3.select(this).transition().duration(100).style('stroke-opacity', 0).style('stroke', 'white');
-      d3.select("body").style('cursor', 'default');
-      return tooltip.text(defaultTooltipText);
-    };
-
-    var popIndicator = function (pos, colormap, w, h) {
-      return function (d,i) {
-        d3.select(this).selectAll(".cell")
-          .data(d)
-          .enter().append("rect")
-          .attr("class", "cell")
-          .attr(pos, function (d, i) {
-            return x(i);
-          })
-          .attr("width", w)
-          .attr("height", h)
-          .attr("title", function (d) {
-            return d.id;
-          })
-          .style("fill", function (d) {
-            return colormap(popNameFromId(d.id));
-          })
-          .style("stroke", function (d) {
-            return colormap(popNameFromId(d.id));
-          })
-          .on("mouseover", function (d){
-            $.proxy(mouseoverCell, this)(popNameFromId(d.id))
-          })
-          .on("mouseout", $.proxy(mouseoutCell))
-      };
-    };
-
-    var colormap = this.connectivity.nodeColormap.range ? this.connectivity.nodeColormap : d3.scaleOrdinal(d3.schemeCategory20);
-    var postMargin = parseInt(rect.attr("width")) / pre.length;
-    var preMargin = parseInt(rect.attr("height")) / post.length;
-
-    var postPop = container.selectAll(".postPop")
-      .data([post])
-      .enter()
-      .append("g")
-      .attr("class", "postPop")
-      .attr("transform", "translate(0,-10)")
-      .each(popIndicator("x", colormap, postMargin, 5));
-    var prePop = container.selectAll(".prePop")
-      .data([pre])
-      .enter()
-      .append("g")
-      .attr("class", "prePop")
-      .attr("transform", "translate(-10,0)")
-      .each(popIndicator("y", colormap, 5, preMargin));
-
-    var row = container.selectAll(".row")
-      .data(matrix)
-      .enter().append("g")
-      .attr("class", "row")
-      .attr("transform", function (d, i) {
-        return "translate(0," + x(i) + ")";
-      })
-      .each(row);
-
-    row.append("line")
-      .attr("x2", this.connectivity.innerWidth);
-
-    var column = container.selectAll(".column")
-      .data(matrix)
-      .enter().append("g")
-      .attr("class", "column")
-      .attr("transform", function (d, i) {
-        return "translate(" + x(i) + ")rotate(-90)";
-      });
-
-    column.append("line")
-      .attr("x1", -this.connectivity.innerWidth);
-
-    this.connectivity.createLegend('legend', colormap, { x: matrixDim, y: 0 });
-
-    // Sorting matrix entries by criteria specified via combobox
-    var orderContainer = $('<div/>', {
-      id: this.connectivity.id + '-ordering',
-      style: 'width:' + legendWidth + 'px;left:' + (matrixDim + this.connectivity.widgetMargin) + 'px;top:' + (matrixDim - 32) + 'px;',
-      class: 'connectivity-ordering'
-    }).appendTo(this.connectivity.connectivityContainer);
-
-    var orderCombo = $('<select/>');
-    $.each(sortOptions, function (k, v) {
-      $('<option/>', { value: k, text: v }).appendTo(orderCombo);
-    });
-    orderContainer.append($('<span/>', {
-      id: 'matrix-sorter',
-      class: 'connectivity-ordering-label',
-      text: 'Order by:'
-    }).append(orderCombo));
-
+    // let matrix = [];
+    // let nodes = this.connectivity.dataset.nodes;
+    // const root = this.connectivity.dataset.root;
+    // const n = nodes.length;
+    //
+    // // Compute index per node.
+    // nodes.forEach(function (node, i) {
+    //   node.pre_count = 0;
+    //   node.post_count = 0;
+    //   matrix[i] = d3.range(n).map(function (j) {
+    //     return { x: j, y: i, z: 0 };
+    //   });
+    // });
+    //
+    // // Convert links to matrix; count pre / post conns.
+    // this.connectivity.dataset.links.forEach(function (link) {
+    //   /*
+    //    * TODO: think about zero weight lines
+    //    * matrix[link.source][link.target].z = link.weight ? link.type : 0;
+    //    */
+    //   matrix[link.source][link.target].z = link.type;
+    //   nodes[link.source].pre_count += 1;
+    //   nodes[link.target].post_count += 1;
+    // });
+    //
+    // /*
+    //  *Sorting matrix entries.
+    //  *TODO: runtime specified sorting criteria
+    //  */
+    // const sortOptions = {
+    //   'id': 'By entity name',
+    //   'pre_count': 'By # pre',
+    //   'post_count': 'By # post'
+    // };
+    // //  Precompute the orders.
+    // const orders = {
+    //   id: d3.range(n).sort(function (a, b) {
+    //     return d3.ascending(nodes[a].id, nodes[b].id);
+    //   }),
+    //   pre_count: d3.range(n).sort(function (a, b) {
+    //     return nodes[b].pre_count - nodes[a].pre_count;
+    //   }),
+    //   post_count: d3.range(n).sort(function (a, b) {
+    //     return nodes[b].post_count - nodes[a].post_count;
+    //   }),
+    //   // community: d3.range(n).sort(function(a, b) { return nodes[b].community - nodes[a].community; }),
+    // };
+    // // Default sort order.
+    // x.domain(orders.id);
+    //
+    // const rect = container
+    //   .append("rect")
+    //   .attr("class", "background")
+    //   .attr("width", matrixDim - margin.left - 30)
+    //   .attr("height", matrixDim - margin.top);
+    //
+    // /*
+    //  * we store the 'conn' key in case we want to
+    //  * eg. conditionally colour the indicator if there
+    //  * are actually connections in that row/column
+    //  */
+    // const pre = nodes.map(function (x, i) {
+    //   return {
+    //     id: x.id, conn: matrix[i].filter(function (d) {
+    //       return d.z;
+    //     }).length > 0
+    //   }
+    // });
+    // const matrixT = matrix[0].map(function (col, i) {
+    //   return matrix.map(function (row) {
+    //     return row[i];
+    //   })
+    // });
+    // const post = nodes.map(function (x, i) {
+    //   return {
+    //     id: x.id, conn: matrixT[i].filter(function (d) {
+    //       return d.z;
+    //     }).length > 0
+    //   }
+    // });
+    //
+    // const popNameFromId = function (id) {
+    //   return eval(GEPPETTO.ModelFactory.getAllPotentialInstancesEndingWith(id)[0]).getParent().getName();
+    // };
+    //
+    // const mouseoverCell = function (msg) {
+    //   d3.select(this.parentNode.appendChild(this)).transition().duration(100).style('stroke-opacity', 1).style('stroke', 'white').style('stroke-width', 2);
+    //   d3.select("body").style('cursor', 'pointer');
+    //   return tooltip.transition().duration(100).text(msg);
+    // };
+    //
+    // const mouseoutCell = function () {
+    //   d3.select(this).transition().duration(100).style('stroke-opacity', 0).style('stroke', 'white');
+    //   d3.select("body").style('cursor', 'default');
+    //   return tooltip.text(defaultTooltipText);
+    // };
+    //
+    // const popIndicator = function (pos, colormap, w, h) {
+    //   return function (d, i) {
+    //     d3.select(this).selectAll(".cell")
+    //       .data(d)
+    //       .enter().append("rect")
+    //       .attr("class", "cell")
+    //       .attr(pos, function (d, i) {
+    //         return x(i);
+    //       })
+    //       .attr("width", w)
+    //       .attr("height", h)
+    //       .attr("title", function (d) {
+    //         return d.id;
+    //       })
+    //       .style("fill", function (d) {
+    //         return colormap(popNameFromId(d.id));
+    //       })
+    //       .style("stroke", function (d) {
+    //         return colormap(popNameFromId(d.id));
+    //       })
+    //       .on("mouseover", function (d) {
+    //         $.proxy(mouseoverCell, this)(popNameFromId(d.id))
+    //       })
+    //       .on("mouseout", $.proxy(mouseoutCell))
+    //   };
+    // };
+    //
+    // const colormap = this.connectivity.nodeColormap.range ? this.connectivity.nodeColormap : d3.scaleOrdinal(d3.schemeCategory20);
+    // const postMargin = parseInt(rect.attr("width")) / pre.length;
+    // const preMargin = parseInt(rect.attr("height")) / post.length;
+    //
+    // const postPop = container.selectAll(".postPop")
+    //   .data([post])
+    //   .enter()
+    //   .append("g")
+    //   .attr("class", "postPop")
+    //   .attr("transform", "translate(0,-10)")
+    //   .each(popIndicator("x", colormap, postMargin, 5));
+    // const prePop = container.selectAll(".prePop")
+    //   .data([pre])
+    //   .enter()
+    //   .append("g")
+    //   .attr("class", "prePop")
+    //   .attr("transform", "translate(-10,0)")
+    //   .each(popIndicator("y", colormap, 5, preMargin));
+    //
+    // const row = container.selectAll(".row")
+    //   .data(matrix)
+    //   .enter().append("g")
+    //   .attr("class", "row")
+    //   .attr("transform", function (d, i) {
+    //     return "translate(0," + x(i) + ")";
+    //   })
+    //   .each(row);
+    //
+    // row.append("line")
+    //   .attr("x2", this.connectivity.innerWidth);
+    //
+    // const column = container.selectAll(".column")
+    //   .data(matrix)
+    //   .enter().append("g")
+    //   .attr("class", "column")
+    //   .attr("transform", function (d, i) {
+    //     return "translate(" + x(i) + ")rotate(-90)";
+    //   });
+    //
+    // column.append("line")
+    //   .attr("x1", -this.connectivity.innerWidth);
+    //
+    // this.connectivity.createLegend('legend', colormap, { x: matrixDim, y: 0 });
+    //
+    // // Sorting matrix entries by criteria specified via combobox
+    // const orderContainer = this.connectivity.appendTo(this.connectivity.connectivityContainer,
+    //   this.connectivity.createElement('div', {
+    //     id: this.connectivity.id + '-ordering',
+    //     style: 'width:' + legendWidth + 'px;left:' + (matrixDim + this.connectivity.widgetMargin) + 'px;top:' + (matrixDim - 32) + 'px;',
+    //     class: 'connectivity-ordering'
+    //   }));
+    //
+    // const orderCombo = this.connectivity.selectElement('select');
+    // sortOptions.forEach((k, v) => function (k, v) {
+    //   this.connectivity.appendTo(orderCombo, this.connectivity.createElement('option', { value: k, text: v }))
+    // });
+    //
+    // const m = this.connectivity.append(orderContainer, this.connectivity.createElement('span', {
+    //   id: 'matrix-sorter',
+    //   class: 'connectivity-ordering-label',
+    //   text: 'Order by:'
+    // }));
+    //
+    // this.connectivity.append(orderCombo, m)
     
   }
-
-
 }
