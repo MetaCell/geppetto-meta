0ea21659c9ed2f7dc808429659eee81ad477e2d3
Afonso Pinto <afonsobspinto@gmail.com>
2020-03-24 20:53:38 +0000

    Fix flexlayout

--- geppetto-showcase/index.js
+++ geppetto-showcase/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import 'font-awesome/scss/font-awesome.scss'
 import '@geppettoengine/geppetto-client/style/css/gpt-icons.css'
+import '../geppetto-ui/flexLayout2/showcase/styles/flexlayout.less'
 
 
 const rootElement = document.getElementById("container");
--- geppetto-ui/flexLayout2/showcase/FlexLayoutShowcase.js
+++ geppetto-ui/flexLayout2/showcase/FlexLayoutShowcase.js
@@ -2,25 +2,18 @@ import React, { Component } from "react";
 import * as FlexLayout from '../src/index';
 
 const json = {
-    global: {tabEnableClose: false},
+    global: {
+        tabEnableClose: true,
+        tabSetHeaderHeight: 26,
+        tabSetTabStripHeight: 26
+    },
     borders: [
         {
             "type": "border",
             "location": "bottom",
             "size": 100,
-            "children": [
-                {
-                    "type": "tab",
-                    "name": "four",
-                    "component": "text"
-                }
-            ]
-        },
-        {
-            "type": "border",
-            "location": "left",
-            "size": 100,
-            "children": []
+            "children": [],
+            "barSize": 35
         }
     ],
     layout: {
@@ -28,37 +21,55 @@ const json = {
         "weight": 100,
         "children": [
             {
-                "type": "tabset",
-                "weight": 50,
+                "type": "row",
+                "weight": 55,
                 "selected": 0,
                 "children": [
                     {
-                        "type": "tab",
-                        "name": "One",
-                        "component": "text"
+                        "type": "tabset",
+                        "weight": 36,
+                        "children": [
+                            {
+                                "type": "tab",
+                                "name": "Tab One",
+                                "component": "text"
+                            }
+                        ]
+                    },
+                    {
+                        "type": "tabset",
+                        "weight": 64,
+                        "children": [
+                            {
+                                "type": "tab",
+                                "name": "Tab Two",
+                                "component": "text"
+                            }
+                        ]
                     }
                 ]
             },
             {
                 "type": "tabset",
-                "weight": 50,
+                "weight": 45,
                 "selected": 0,
                 "children": [
                     {
                         "type": "tab",
-                        "name": "Two",
+                        "name": "Tab Three",
                         "component": "text"
                     },
                     {
                         "type": "tab",
-                        "name": "Three",
+                        "name": "Tab Four",
                         "component": "text"
                     }
                 ]
-            }
+            },
         ]
     }
 };
+
 
 export default class FlexLayoutShowcase extends Component {
 
@@ -68,15 +79,12 @@ export default class FlexLayoutShowcase extends Component {
     }
 
     factory(node) {
-        const component = node.getComponent();
-        if (component === "text") {
-            return (<div className="panel">Panel {node.getName()}</div>);
-        }
+        return (<div className="flexChildContainer">Content {node.getName()}</div>);
     }
 
     render() {
         return (
-            <div style={{position:"absolute", top:"100px", width:"100%"}}>
+            <div style={{ position:"relative", height: '100%'}}>
                 <FlexLayout.Layout
                     model={this.state.model}
                     factory={this.factory.bind(this)}/>
