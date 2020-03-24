5d5d8e13012936204d5185c5a20a4a087eecbf9f
Afonso Pinto <afonsobspinto@gmail.com>
2020-03-24 14:58:48 +0000

    Fix geppetto icons

--- geppetto-showcase/index.js
+++ geppetto-showcase/index.js
@@ -2,6 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import 'font-awesome/scss/font-awesome.scss'
+import '@geppettoengine/geppetto-client/style/css/gpt-icons.css'
+
 
 const rootElement = document.getElementById("container");
 ReactDOM.render(<App />, rootElement);
