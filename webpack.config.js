a9a4499e9bd616bdba6667e9d4ebfeafdb8e3a2c
Afonso Pinto <afonsobspinto@gmail.com>
2020-04-23 20:06:18 +0100

    Add favicon #219

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,17 @@
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
 module.exports = {
-  entry: path.resolve(__dirname, "./geppetto-showcase/index.js"),
-  mode: "development",
-  devtool: "inline-source-map",
+  entry: path.resolve(__dirname, './geppetto-showcase/index.js'),
+  mode: 'development',
+  devtool: 'inline-source-map',
   resolve: {
-    alias: { "@geppettoengine/geppetto-client": path.resolve(__dirname, "./geppetto-client") },
+    alias: {
+      '@geppettoengine/geppetto-client': path.resolve(
+        __dirname,
+        './geppetto-client'
+      ),
+    },
     extensions: ['*', '.js', '.json', '.ts', '.tsx', '.jsx'],
   },
   module: {
@@ -16,53 +21,74 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-          presets: [['@babel/preset-env', { "modules": false }], '@babel/preset-react',
-          { 'plugins': ['@babel/plugin-proposal-class-properties'] }]
-        }
+          presets: [
+            ['@babel/preset-env', { modules: false }],
+            '@babel/preset-react',
+            { plugins: ['@babel/plugin-proposal-class-properties'] },
+          ],
+        },
       },
       {
         test: /\.tsx?$/,
-        loader: "awesome-typescript-loader"
+        loader: 'awesome-typescript-loader',
       },
       {
         test: /\.html$/,
-        use: [
-          { loader: "html-loader" }
-        ]
+        use: [{ loader: 'html-loader' }],
       },
       {
         test: /\.svg/,
         use: {
           loader: 'svg-url-loader',
-          options: {}
-        }
+          options: {},
+        },
       },
       {
         test: /\.less$/,
-        loader: 'style-loader!css-loader!less-loader'
+        loader: 'style-loader!css-loader!less-loader',
       },
-      { test: /\.s[a|c]ss$/, use: [{ loader: "style-loader" }, { loader: "css-loader" }, { loader: "sass-loader" }] },
-      { test: /\.(png|gif|jpg|cur)$/i, loader: 'url-loader', options: { limit: 8192 } },
-      { test: /\.woff2(\?v=[0-9]\.[0-9]\.[0-9])?$/i, loader: 'url-loader', options: { limit: 10000, mimetype: 'application/font-woff2' } },
-      { test: /\.woff(\?v=[0-9]\.[0-9]\.[0-9])?$/i, loader: 'url-loader', options: { limit: 10000, mimetype: 'application/font-woff' } },
-      { test: /\.(ttf|eot|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/i, loader: 'file-loader' },
+      {
+        test: /\.s[a|c]ss$/,
+        use: [
+          { loader: 'style-loader' },
+          { loader: 'css-loader' },
+          { loader: 'sass-loader' },
+        ],
+      },
+      {
+        test: /\.(png|gif|jpg|cur)$/i,
+        loader: 'url-loader',
+        options: { limit: 8192 },
+      },
+      {
+        test: /\.woff2(\?v=[0-9]\.[0-9]\.[0-9])?$/i,
+        loader: 'url-loader',
+        options: { limit: 10000, mimetype: 'application/font-woff2' },
+      },
+      {
+        test: /\.woff(\?v=[0-9]\.[0-9]\.[0-9])?$/i,
+        loader: 'url-loader',
+        options: { limit: 10000, mimetype: 'application/font-woff' },
+      },
+      {
+        test: /\.(ttf|eot|otf)(\?v=[0-9]\.[0-9]\.[0-9])?$/i,
+        loader: 'file-loader',
+      },
       {
         test: /\.css$/,
-        loader: 'style-loader!css-loader'
+        loader: 'style-loader!css-loader',
       },
       {
         test: /\.md$/,
-        use: [{ loader: "html-loader" }, { loader: "markdown-loader" }
-        ]
+        use: [{ loader: 'html-loader' }, { loader: 'markdown-loader' }],
       },
-
-
-    ]
+    ],
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./geppetto-showcase/index.html",
-      filename: "./index.html"
-    })
-  ]
+      template: './geppetto-showcase/index.html',
+      filename: './index.html',
+      favicon: './geppetto-client/style/favicon.png',
+    }),
+  ],
 };
