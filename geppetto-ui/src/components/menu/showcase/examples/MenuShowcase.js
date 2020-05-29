957ff0161311cde3f1348a6f223c51937e6d83c1
Afonso Pinto <afonsobspinto@gmail.com>
2020-05-29 10:39:36 +0100

    Fix double menu showcase

--- geppetto-ui/src/components/menu/showcase/examples/MenuShowcase.js
+++ geppetto-ui/src/components/menu/showcase/examples/MenuShowcase.js
@@ -1,42 +1,41 @@
-import React, { Component } from "react";
-import Menu from "./../../Menu";
-import model from "./../model.json";
+import React, { Component } from 'react';
+import Menu from './../../Menu';
+import model from './../model.json';
 
 export default class MenuShowcase extends Component {
-  constructor (props) {
+  constructor(props) {
     super(props);
-    this.menuConfiguration = require('./../menuConfiguration').toolbarMenu;
+    this.menuConfiguration = require('../menuConfiguration').default;
     this.menuHandler = this.menuHandler.bind(this);
   }
 
   /**
-   * Handler function triggered when a Menu item is clicked. 
+   * Handler function triggered when a Menu item is clicked.
    */
-  menuHandler (click) {
+  menuHandler(click) {
     const historyList = [];
     console.log(click.handlerAction);
     // Check the handlerAction associated with Menu item clicked
     if (click.handlerAction === 'historyMenuInjector') {
       // Add to history List.
-      historyList.push(
-        {
-          label: "adult brain template JFRC2",
-          icon: "",
-          action: {
-            handlerAction: "triggerSetTermInfo",
-            value: console.log("adult brain template JFRC2")
-          }
+      historyList.push({
+        label: 'adult brain template JFRC2',
+        icon: '',
+        action: {
+          handlerAction: 'triggerSetTermInfo',
+          value: console.log('adult brain template JFRC2'),
         },
-      );
+      });
     }
     return historyList;
   }
 
-  render () {
+  render() {
     return (
       <Menu
         configuration={this.menuConfiguration}
-        menuHandler={this.menuHandler}/>
+        menuHandler={this.menuHandler}
+      />
     );
   }
 }
