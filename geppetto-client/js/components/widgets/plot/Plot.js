8ee64638894ebf3082d754ae851a9dd92b4d8f03
jrmartin <yeshuarmartinez@gmail.com>
2020-06-02 17:43:33 -0700

    use parser.eval for mathjs call

--- geppetto-client/js/components/widgets/plot/Plot.js
+++ geppetto-client/js/components/widgets/plot/Plot.js
@@ -1058,7 +1058,7 @@ define(function (require) {
 
       // Parse func as a mathjs object
       var parser = math.parser();
-      var mathFunc = parser.evaluate(func);
+      var mathFunc = parser.eval(func);
       var data = [];
       data.name = options.legendText;
       data.data = {};
