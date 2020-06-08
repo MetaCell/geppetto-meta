32923c44f1abaca72c07cfcfd95b13876b3f2dff
Rob Court <rcourt@ed.ac.uk>
2020-06-08 15:53:01 +0100

    blocking during loading

--- js/components/widgets/stackViewer/StackViewerComponent.js
+++ js/components/widgets/stackViewer/StackViewerComponent.js
@@ -503,81 +503,83 @@ define(function (require) {
         xhrType: PIXI.loaders.Resource.XHR_RESPONSE_TYPE.BLOB
       };
 
-      for (j = 0; j < this.state.numTiles; j++) {
-        for (i in this.state.stack) {
-          image = this.state.serverUrl.toString() + '?wlz=' + this.state.stack[i] + '&sel=0,255,255,255&mod=zeta&fxp=' + this.props.fxp.join(',') + '&scl=' + Number(this.state.scl).toFixed(1) + '&dst=' + Number(this.state.dst).toFixed(1) + '&pit=' + Number(this.state.pit).toFixed(0) + '&yaw=' + Number(this.state.yaw).toFixed(0) + '&rol=' + Number(this.state.rol).toFixed(0) + '&qlt=80&jtl=' + j.toString();
-          if (!imageLoader.resources[image]) {
-            // console.log('buffering ' + this.state.stack[i].toString() + '...');
-            imageLoader.add(image, image, loaderOptions);
-            buffMax -= 1;
-          }
-        }
-      }
-      if (buffMax < 2000 || this.state.lastUpdate < (Date.now() - 600000)) { // only buffer surounding tiles if the main image has changed or 10 minute has passed
-        this.state.lastUpdate = Date.now();
-        var step;
-        if (this.state.orth == 0) {
-          step = this.state.voxelZ * this.state.scl;
-        } else if (this.state.orth == 1) {
-          step = this.state.voxelY * this.state.scl;
-        } else if (this.state.orth == 2) {
-          step = this.state.voxelX * this.state.scl;
-        }
-        if (this.state.numTiles < 10) {
-          for (maxDist = Number(this.state.dst).toFixed(1); maxDist < max; maxDist += step) {
-            for (i in this.state.stack) {
-              for (j in this.state.visibleTiles) {
-                image = this.state.serverUrl.toString() + '?wlz=' + this.state.stack[i] + '&sel=0,255,255,255&mod=zeta&fxp=' + this.props.fxp.join(',') + '&scl=' + Number(this.state.scl).toFixed(1) + '&dst=' + Number(maxDist).toFixed(1) + '&pit=' + Number(this.state.pit).toFixed(0) + '&yaw=' + Number(this.state.yaw).toFixed(0) + '&rol=' + Number(this.state.rol).toFixed(0) + '&qlt=80&jtl=' + this.state.visibleTiles[j].toString();
-                if (dst < max && !imageLoader.resources[image]) {
-                  imageLoader.add(image, image, loaderOptions);
-                }
-                buffMax -= 1;
-              }
-            }
-            if (buffMax < 1000) {
-              break;
-            }
-          }
-          for (maxDist = Number(this.state.dst).toFixed(1); maxDist > min; maxDist -= step) {
-            for (i in this.state.stack) {
-              for (j in this.state.visibleTiles) {
-                image = this.state.serverUrl.toString() + '?wlz=' + this.state.stack[i] + '&sel=0,255,255,255&mod=zeta&fxp=' + this.props.fxp.join(',') + '&scl=' + Number(this.state.scl).toFixed(1) + '&dst=' + Number(maxDist).toFixed(1) + '&pit=' + Number(this.state.pit).toFixed(0) + '&yaw=' + Number(this.state.yaw).toFixed(0) + '&rol=' + Number(this.state.rol).toFixed(0) + '&qlt=80&jtl=' + this.state.visibleTiles[j].toString();
-                if (dst < max && !imageLoader.resources[image]) {
-                  imageLoader.add(image, image, loaderOptions);
-                }
-                buffMax -= 1;
-              }
-            }
-            if (buffMax < 1) {
-              break;
-            }
-          }
-        } else {
-          // console.log('Buffering neighbouring layers (' + this.state.numTiles.toString() + ') tiles...');
-          for (j = 0; j < this.state.numTiles; j++) {
-            for (i in this.state.stack) {
-              image = this.state.serverUrl.toString() + '?wlz=' + this.state.stack[i] + '&sel=0,255,255,255&mod=zeta&fxp=' + this.props.fxp.join(',') + '&scl=' + Number(this.state.scl).toFixed(1) + '&dst=' + Number(this.state.dst).toFixed(1) + '&pit=' + Number(this.state.pit).toFixed(0) + '&yaw=' + Number(this.state.yaw).toFixed(0) + '&rol=' + Number(this.state.rol).toFixed(0) + '&qlt=80&jtl=' + j.toString();
-              if (!imageLoader.resources[image]) {
-                // console.log('buffering ' + this.state.stack[i].toString() + '...');
-                imageLoader.add(image, image, loaderOptions);
-              }
+      if (!imageLoader.loading) {
+        for (j = 0; j < this.state.numTiles; j++) {
+          for (i in this.state.stack) {
+            image = this.state.serverUrl.toString() + '?wlz=' + this.state.stack[i] + '&sel=0,255,255,255&mod=zeta&fxp=' + this.props.fxp.join(',') + '&scl=' + Number(this.state.scl).toFixed(1) + '&dst=' + Number(this.state.dst).toFixed(1) + '&pit=' + Number(this.state.pit).toFixed(0) + '&yaw=' + Number(this.state.yaw).toFixed(0) + '&rol=' + Number(this.state.rol).toFixed(0) + '&qlt=80&jtl=' + j.toString();
+            if (!imageLoader.resources[image]) {
+              // console.log('buffering ' + this.state.stack[i].toString() + '...');
+              imageLoader.add(image, image, loaderOptions);
               buffMax -= 1;
+            }
+          }
+        }
+        if (buffMax < 2000 || this.state.lastUpdate < (Date.now() - 600000)) { // only buffer surounding tiles if the main image has changed or 10 minute has passed
+          this.state.lastUpdate = Date.now();
+          var step;
+          if (this.state.orth == 0) {
+            step = this.state.voxelZ * this.state.scl;
+          } else if (this.state.orth == 1) {
+            step = this.state.voxelY * this.state.scl;
+          } else if (this.state.orth == 2) {
+            step = this.state.voxelX * this.state.scl;
+          }
+          if (this.state.numTiles < 10) {
+            for (maxDist = Number(this.state.dst).toFixed(1); maxDist < max; maxDist += step) {
+              for (i in this.state.stack) {
+                for (j in this.state.visibleTiles) {
+                  image = this.state.serverUrl.toString() + '?wlz=' + this.state.stack[i] + '&sel=0,255,255,255&mod=zeta&fxp=' + this.props.fxp.join(',') + '&scl=' + Number(this.state.scl).toFixed(1) + '&dst=' + Number(maxDist).toFixed(1) + '&pit=' + Number(this.state.pit).toFixed(0) + '&yaw=' + Number(this.state.yaw).toFixed(0) + '&rol=' + Number(this.state.rol).toFixed(0) + '&qlt=80&jtl=' + this.state.visibleTiles[j].toString();
+                  if (dst < max && !imageLoader.resources[image]) {
+                    imageLoader.add(image, image, loaderOptions);
+                  }
+                  buffMax -= 1;
+                }
+              }
+              if (buffMax < 1000) {
+                break;
+              }
+            }
+            for (maxDist = Number(this.state.dst).toFixed(1); maxDist > min; maxDist -= step) {
+              for (i in this.state.stack) {
+                for (j in this.state.visibleTiles) {
+                  image = this.state.serverUrl.toString() + '?wlz=' + this.state.stack[i] + '&sel=0,255,255,255&mod=zeta&fxp=' + this.props.fxp.join(',') + '&scl=' + Number(this.state.scl).toFixed(1) + '&dst=' + Number(maxDist).toFixed(1) + '&pit=' + Number(this.state.pit).toFixed(0) + '&yaw=' + Number(this.state.yaw).toFixed(0) + '&rol=' + Number(this.state.rol).toFixed(0) + '&qlt=80&jtl=' + this.state.visibleTiles[j].toString();
+                  if (dst < max && !imageLoader.resources[image]) {
+                    imageLoader.add(image, image, loaderOptions);
+                  }
+                  buffMax -= 1;
+                }
+              }
               if (buffMax < 1) {
                 break;
               }
             }
-            if (buffMax < 1) {
-              break;
+          } else {
+            // console.log('Buffering neighbouring layers (' + this.state.numTiles.toString() + ') tiles...');
+            for (j = 0; j < this.state.numTiles; j++) {
+              for (i in this.state.stack) {
+                image = this.state.serverUrl.toString() + '?wlz=' + this.state.stack[i] + '&sel=0,255,255,255&mod=zeta&fxp=' + this.props.fxp.join(',') + '&scl=' + Number(this.state.scl).toFixed(1) + '&dst=' + Number(this.state.dst).toFixed(1) + '&pit=' + Number(this.state.pit).toFixed(0) + '&yaw=' + Number(this.state.yaw).toFixed(0) + '&rol=' + Number(this.state.rol).toFixed(0) + '&qlt=80&jtl=' + j.toString();
+                if (!imageLoader.resources[image]) {
+                  // console.log('buffering ' + this.state.stack[i].toString() + '...');
+                  imageLoader.add(image, image, loaderOptions);
+                }
+                buffMax -= 1;
+                if (buffMax < 1) {
+                  break;
+                }
+              }
+              if (buffMax < 1) {
+                break;
+              }
             }
           }
+          imageLoader
+          .on('progress', loadProgressHandler.bind(this))
+          .on('error', console.error)
+          .on('complete', setup.bind(this))
+          .load();
+          console.log('Buffered ' + (2000 - buffMax).toFixed(0) + ' Slice Tiles');
+          this.state.lastUpdate = Date.now();
         }
-        imageLoader
-        .on('progress', loadProgressHandler.bind(this))
-        .on('error', console.error)
-        .on('complete', setup.bind(this))
-        .load();
-        console.log('Buffered ' + (2000 - buffMax).toFixed(0) + ' Slice Tiles');
-        this.state.lastUpdate = Date.now();
       }
 
       function loadProgressHandler (loader, resource) {
