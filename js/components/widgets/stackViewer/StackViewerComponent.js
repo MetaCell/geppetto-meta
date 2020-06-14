c9a4941da6f2be132577e7801daa329c7d68fa32
Rob Court <rcourt@ed.ac.uk>
2020-06-14 09:24:30 +0100

    blocking if no change

--- js/components/widgets/stackViewer/StackViewerComponent.js
+++ js/components/widgets/stackViewer/StackViewerComponent.js
@@ -140,6 +140,51 @@ define(function (require) {
       this.callPlaneEdges();
     },
 
+    shouldComponentUpdate: function(nextProps, nextState) {
+      return shallowCompare(this, nextProps, nextState);
+
+      /**
+       * Performs equality by iterating through keys on an object and returning false
+       * when any key has values which are not strictly equal between the arguments.
+       * Returns true when the values of all keys are strictly equal.
+       */
+      function shallowEqual(objA, objB) {
+        if (objA === objB) {
+          return true;
+        }
+
+        if (typeof objA !== 'object' || objA === null ||
+            typeof objB !== 'object' || objB === null) {
+          return false;
+        }
+
+        var keysA = Object.keys(objA);
+        var keysB = Object.keys(objB);
+
+        if (keysA.length !== keysB.length) {
+          return false;
+        }
+
+        // Test for A's keys different from B.
+        var bHasOwnProperty = hasOwnProperty.bind(objB);
+        for (var i = 0; i < keysA.length; i++) {
+          if (!bHasOwnProperty(keysA[i]) || objA[keysA[i]] !== objB[keysA[i]]) {
+            return false;
+          }
+        }
+
+        return true;
+      }
+
+      function shallowCompare(instance, nextProps, nextState) {
+        return (
+          !shallowEqual(instance.props, nextProps) ||
+          !shallowEqual(instance.state, nextState)
+        );
+      }
+    },
+
+
     componentWillUnmount: function () {
       this.stage.destroy(true);
       this.stage = null;
@@ -1130,6 +1175,50 @@ define(function (require) {
 
         }
 
+      }
+    },
+
+    shouldComponentUpdate: function(nextProps, nextState) {
+      return shallowCompare(this, nextProps, nextState);
+
+      /**
+       * Performs equality by iterating through keys on an object and returning false
+       * when any key has values which are not strictly equal between the arguments.
+       * Returns true when the values of all keys are strictly equal.
+       */
+      function shallowEqual(objA, objB) {
+        if (objA === objB) {
+          return true;
+        }
+
+        if (typeof objA !== 'object' || objA === null ||
+            typeof objB !== 'object' || objB === null) {
+          return false;
+        }
+
+        var keysA = Object.keys(objA);
+        var keysB = Object.keys(objB);
+
+        if (keysA.length !== keysB.length) {
+          return false;
+        }
+
+        // Test for A's keys different from B.
+        var bHasOwnProperty = hasOwnProperty.bind(objB);
+        for (var i = 0; i < keysA.length; i++) {
+          if (!bHasOwnProperty(keysA[i]) || objA[keysA[i]] !== objB[keysA[i]]) {
+            return false;
+          }
+        }
+
+        return true;
+      }
+
+      function shallowCompare(instance, nextProps, nextState) {
+        return (
+          !shallowEqual(instance.props, nextProps) ||
+          !shallowEqual(instance.state, nextState)
+        );
       }
     },
 
