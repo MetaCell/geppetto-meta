0af2b9905f997f02450fa9a3030e274307ff5ce7
Dario Del Piano <dario@metacell.us>
2020-09-28 10:59:49 +0100

    showcase, defining the proptypes and descriptions

--- geppetto-ui/src/search/showcase/Search.js
+++ geppetto-ui/src/search/showcase/Search.js
@@ -12,10 +12,25 @@ class SearchPropTypes extends React.Component {
 
 SearchPropTypes.propTypes = {
   /**
-   * Test proptypes for FlexLayout
-   * 
+   * String that defines the datasource to be used
    */
-  test: PropTypes.bool,
+  datasource: PropTypes.string.isRequired,
+  /**
+   * json configuration that defines all the configurable elements of the component
+   */
+  searchConfiguration: PropTypes.any.isRequired,
+  /**
+   * json configuration to set all the information required by the datasource (e.g. query, pagination, etc)
+   */
+  datasourceConfiguration: PropTypes.any.isRequired,
+  /**
+   * json style configuration used to defined the component style
+   */
+  searchStyle: PropTypes.any,
+  /**
+   * if the string used is CUSTOM then we can define our own datasource and pass it as a function using this prop
+   */
+  customDatasourceHandler: PropTypes.func
 };
 
 export default SearchPropTypes;
