947523c2796729ddad2b3437fb366de82a497371
Dario Del Piano <dario@metacell.us>
2021-06-30 12:25:55 +0100

    #103 custom changes applied for geppetto codebase

--- geppetto.js/geppetto-ui/src/flex-layout/src/model/Node.ts
+++ geppetto.js/geppetto-ui/src/flex-layout/src/model/Node.ts
@@ -1,22 +1,20 @@
-import Rect from "../Rect";
-import Model from "./Model";
 import AttributeDefinitions from "../AttributeDefinitions";
-import Orientation from "../Orientation";
 import DockLocation from "../DockLocation";
 import DropInfo from "../DropInfo";
-import { JSMap } from "../Types";
+import Orientation from "../Orientation";
+import Rect from "../Rect";
 import IDraggable from "./IDraggable";
+import Model, { ILayoutMetrics } from "./Model";
 
 abstract class Node {
-
     /** @hidden @internal */
     protected _model: Model;
     /** @hidden @internal */
-    protected _attributes: JSMap<any>;
+    protected _attributes: Record<string, any>;
     /** @hidden @internal */
     protected _parent?: Node;
     /** @hidden @internal */
-    protected _children: Array<Node>;
+    protected _children: Node[];
     /** @hidden @internal */
     protected _fixed: boolean;
     /** @hidden @internal */
@@ -24,7 +22,7 @@ abstract class Node {
     /** @hidden @internal */
     protected _visible: boolean;
     /** @hidden @internal */
-    protected _listeners: JSMap<(params: any) => void>;
+    protected _listeners: Record<string, (params: any) => void>;
     /** @hidden @internal */
     protected _dirty: boolean = false;
     /** @hidden @internal */
@@ -41,26 +39,8 @@ abstract class Node {
         this._listeners = {};
     }
 
-    /** @hidden @internal */
-    protected _getAttributeAsStringOrUndefined(attr: string) {
-        const value = this._attributes[attr];
-        if (value !== undefined) {
-            return value as string;
-        }
-        return undefined;
-    }
-
-    /** @hidden @internal */
-    protected _getAttributeAsNumberOrUndefined(attr: string) {
-        const value = this._attributes[attr];
-        if (value !== undefined) {
-            return value as number;
-        }
-        return undefined;
-    }
-
     getId() {
-        let id = this._attributes["id"];
+        let id = this._attributes.id;
         if (id !== undefined) {
             return id as string;
         }
@@ -76,7 +56,7 @@ abstract class Node {
     }
 
     getType() {
-        return this._attributes["type"] as string;
+        return this._attributes.type as string;
     }
 
     getParent() {
@@ -97,9 +77,8 @@ abstract class Node {
 
     getOrientation(): Orientation {
         if (this._parent === undefined) {
-            return Orientation.HORZ;
-        }
-        else {
+            return this._model.isRootOrientationVertical() ? Orientation.VERT : Orientation.HORZ;
+        } else {
             return Orientation.flip(this._parent.getOrientation());
         }
     }
@@ -115,12 +94,12 @@ abstract class Node {
 
     /** @hidden @internal */
     _setId(id: string) {
-        this._attributes["id"] = id;
+        this._attributes.id = id;
     }
 
     /** @hidden @internal */
     _fireEvent(event: string, params: any) {
-        //console.log(this._type, " fireEvent " + event + " " + JSON.stringify(params));
+        // console.log(this._type, " fireEvent " + event + " " + JSON.stringify(params));
         if (this._listeners[event] !== undefined) {
             this._listeners[event](params);
         }
@@ -131,13 +110,13 @@ abstract class Node {
         let val = this._attributes[name];
 
         if (val === undefined) {
-            let modelName = this._getAttributeDefinitions().getModelName(name);
+            const modelName = this._getAttributeDefinitions().getModelName(name);
             if (modelName !== undefined) {
                 val = this._model._getAttribute(modelName);
             }
         }
 
-        //console.log(name + "=" + val);
+        // console.log(name + "=" + val);
         return val;
     }
 
@@ -147,20 +126,19 @@ abstract class Node {
         level++;
         this._children.forEach((node) => {
             node._forEachNode(fn, level);
-        })
+        });
     }
-
 
     /** @hidden @internal */
     _setVisible(visible: boolean) {
         if (visible !== this._visible) {
-            this._fireEvent("visibility", { visible: visible });
+            this._fireEvent("visibility", { visible });
             this._visible = visible;
         }
     }
 
     /** @hidden @internal */
-    _getDrawChildren(): Array<Node> | undefined{
+    _getDrawChildren(): Node[] | undefined {
         return this._children;
     }
 
@@ -176,12 +154,12 @@ abstract class Node {
 
     /** @hidden @internal */
     _setWeight(weight: number) {
-        this._attributes["weight"] = weight;
+        this._attributes.weight = weight;
     }
 
     /** @hidden @internal */
     _setSelected(index: number) {
-        this._attributes["selected"] = index;
+        this._attributes.selected = index;
     }
 
     /** @hidden @internal */
@@ -190,19 +168,18 @@ abstract class Node {
     }
 
     /** @hidden @internal */
-    _layout(rect: Rect) {
+    _layout(rect: Rect, metrics: ILayoutMetrics) {
         this._rect = rect;
     }
 
     /** @hidden @internal */
-    _findDropTargetNode(dragNode: (Node & IDraggable), x: number, y: number): DropInfo | undefined {
-        let rtn: DropInfo | undefined = undefined;
+    _findDropTargetNode(dragNode: Node & IDraggable, x: number, y: number): DropInfo | undefined {
+        let rtn: DropInfo | undefined;
         if (this._rect.contains(x, y)) {
             rtn = this.canDrop(dragNode, x, y);
             if (rtn === undefined) {
                 if (this._children.length !== 0) {
-                    for (let i = 0; i < this._children.length; i++) {
-                        const child = this._children[i];
+                    for (const child of this._children) {
                         rtn = child._findDropTargetNode(dragNode, x, y);
                         if (rtn !== undefined) {
                             break;
@@ -216,13 +193,13 @@ abstract class Node {
     }
 
     /** @hidden @internal */
-    canDrop(dragNode: (Node & IDraggable), x: number, y: number): DropInfo | undefined {
+    canDrop(dragNode: Node & IDraggable, x: number, y: number): DropInfo | undefined {
         return undefined;
     }
 
     /** @hidden @internal */
-    _canDockInto(dragNode: (Node & IDraggable), dropInfo: DropInfo | undefined): boolean {
-        if (dropInfo != undefined) {
+    _canDockInto(dragNode: Node & IDraggable, dropInfo: DropInfo | undefined): boolean {
+        if (dropInfo != null) {
             if (dropInfo.location === DockLocation.CENTER && dropInfo.node.isEnableDrop() === false) {
                 return false;
             }
@@ -238,7 +215,7 @@ abstract class Node {
 
             // finally check model callback to check if drop allowed
             if (this._model._getOnAllowDrop()) {
-                return (this._model._getOnAllowDrop() as (dragNode: (Node), dropInfo: DropInfo) => boolean)(dragNode, dropInfo);
+                return (this._model._getOnAllowDrop() as (dragNode: Node, dropInfo: DropInfo) => boolean)(dragNode, dropInfo);
             }
         }
         return true;
@@ -256,10 +233,9 @@ abstract class Node {
 
     /** @hidden @internal */
     _addChild(childNode: Node, pos?: number) {
-        if (pos != undefined) {
+        if (pos != null) {
             this._children.splice(pos, 0, childNode);
-        }
-        else {
+        } else {
             this._children.push(childNode);
             pos = this._children.length - 1;
         }
@@ -275,11 +251,11 @@ abstract class Node {
     }
 
     /** @hidden @internal */
-    _styleWithPosition(style?: JSMap<any>) {
-        if (style == undefined) {
+    _styleWithPosition(style?: Record<string, any>) {
+        if (style == null) {
             style = {};
         }
-
+        // return this._rect.styleWithPosition(style);
         let tempStyle = this._rect.styleWithPosition(style);
         let sidesValue = this.getModel()._getAttribute("sideBorders");
         if((sidesValue != undefined) && (sidesValue > 0)) {
@@ -318,10 +294,9 @@ abstract class Node {
     /** @hidden @internal */
     abstract _updateAttrs(json: any): void;
     /** @hidden @internal */
-    abstract _getAttributeDefinitions(): AttributeDefinitions ;
+    abstract _getAttributeDefinitions(): AttributeDefinitions;
     /** @hidden @internal */
     abstract _toJson(): any;
-
 }
 
 export default Node;
