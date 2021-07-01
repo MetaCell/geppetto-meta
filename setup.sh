fc0150ca0bd2bb31a70bb618dbc1ac8a621b21b0
Lucas Rebscher <luc.rebscher@gmail.com>
2021-07-01 12:41:13 +0200

    Fix Codefresh build

--- setup.sh
+++ setup.sh
@@ -4,16 +4,12 @@ app=$(pwd)
 
 cd $app/geppetto.js/geppetto-core
 yarn && yarn build && yarn publish:yalc
-cd build && yalc push
-
-cd $app/geppetto.js/geppetto-client
-yarn && yarn build && yarn publish:yalc
-cd build && yalc push
 
 cd $app/geppetto.js/geppetto-ui
 yarn && yarn build && yarn publish:yalc
-cd build && yalc push
+
+cd $app/geppetto.js/geppetto-client
+yarn && yarn build && yarn publish:yalc
 
 cd $app/examples/redux-react-app
-
 yarn && yarn start
