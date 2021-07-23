7e104c482429ffa841ea9bddc2732b3f488f9462
Lucas Rebscher <luc.rebscher@gmail.com>
2021-07-23 16:04:33 +0200

    #129 Use widget config in example app

--- examples/redux-react-app/src/components/MyComponent.js
+++ examples/redux-react-app/src/components/MyComponent.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { Button } from '@material-ui/core';
 
 /**
  * Any custom component you can imagine.
  * 
  * This component is referenced in the `app/componentMap.js`.
  */
-export const MyComponent = () => {
+export const MyComponent = (props) => {
+
+    const { text } = props;
 
     return (
         <div>
-            <Button>Click me</Button>
-            <h1>My Component</h1>
-            <p>Yeah it works</p>
+            <h1>Robert Frost</h1>
+            <p>{text}</p>
         </div>
     )
 }
