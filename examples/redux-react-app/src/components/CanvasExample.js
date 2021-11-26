a660412ab79ea841433cf9870a6a111b06e057d1
Muhaddatha <mzabdulg@umich.edu>
2021-11-26 09:52:06 -0500

    #201 turn canvasExample from class to fucntional component

--- examples/redux-react-app/src/components/CanvasExample.js
+++ examples/redux-react-app/src/components/CanvasExample.js
@@ -1,83 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Canvas from "@metacell/geppetto-meta-ui/3d-canvas/Canvas";
 import CameraControls from "@metacell/geppetto-meta-ui/camera-controls/CameraControls";
-import { withStyles } from '@material-ui/core';
-import { applySelection, mapToCanvasData } from "@metacell/geppetto-meta-ui/3d-canvas/showcase/examples/SelectionUtils";
-import {compose} from 'redux';
-import {connect} from 'react-redux';
+import { makeStyles } from '@material-ui/core';
+import { applySelection, mapToCanvasData } from "@metacell/geppetto-meta-ui/3d-canvas/showcase/utils/SelectionUtils";
+import { connect } from 'react-redux';
 
 
-const styles = () => ({
+const useStyles = makeStyles(() => ({
     canvasContainer: {
         height: '100%',
         width: '100%',
     },
-});
-
-class CanvasExample extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: [],
-            cameraOptions: {
-                angle: 50,
-                near: 0.01,
-                far: 1000,
-                baseZoom: 1,
-                cameraControls: {
-                    instance: CameraControls,
-                    props: { wireframeButtonEnabled: false, },
-                },
-                reset: true,
-                autorotate: false,
-                wireframe: false,
-            },
-        };
-        this.canvasIndex = 3
-        this.lastCameraUpdate = null;
-        this.cameraHandler = this.cameraHandler.bind(this);
-        this.onSelection = this.onSelection.bind(this)
-        this.onMount = this.onMount.bind(this);
-        this.layoutRef = React.createRef();
-    }
-
-    cameraHandler(obj) {
-        this.lastCameraUpdate = obj;
-    }
-
-    onMount(scene) {
-        console.log(scene)
-    }
-
-    onSelection(selectedInstances) {
-        this.setState({ data: applySelection(this.state.data, selectedInstances) })
-    }
-
-    render() {
-        const data = this.props.canvasData;
-        const { cameraOptions } = this.state;
-        const canvasData = mapToCanvasData(data)
-        const { classes } = this.props
-        let camOptions = cameraOptions;
-        if (this.lastCameraUpdate) {
-            camOptions = {
-                ...cameraOptions,
-                position: this.lastCameraUpdate.position,
-                rotation: this.lastCameraUpdate.rotation,
-            };
-        }
-        return <div className={classes.canvasContainer}>
-            <Canvas
-                data={canvasData}
-                cameraOptions={camOptions}
-                cameraHandler={this.cameraHandler}
-                backgroundColor={0x505050}
-                onSelection={this.onSelection}
-                onMount={this.onMount}
-            />
-        </div>
-    }
-}
+}));
 
 const mapStateToProps = state => {
     return {
@@ -85,12 +19,64 @@ const mapStateToProps = state => {
     }
 }
 
-export default compose(
-    connect(
-        mapStateToProps,
-        null
-    ),
-    withStyles(styles),
-  )(CanvasExample);
+function CanvasExample(props) {
+    const style = useStyles();
+    const [data, setData] = React.useState(props.canvasData);
+    const [cameraOptions, setCameraOptions] = React.useState({
+        angle: 50,
+        near: 0.01,
+        far: 1000,
+        baseZoom: 1,
+        cameraControls: {
+            instance: CameraControls,
+            props: {
+                wireframeButtonEnabled: false,
+            }
+        }
+    });
+    const [canvasIndex, setCanvasIndex] = React.useState(3);
+    const [lastCameraUpdate, setLastCameraUpdate] = React.useState(null);
+    const ref = React.useRef();
 
-// export default withStyles(styles)(CanvasExample);
+    const onMount = (scene) => {
+        console.log('scene', scene);
+    }
+
+
+    const cameraHandler = (obj) => {
+        setLastCameraUpdate(obj);
+    }
+
+    const onSelection = (selectedInstances) => {
+        setData(applySelection(data, selectedInstances));
+    }
+
+    const canvasData = mapToCanvasData(data);
+    const classes = props.classes;
+    let camOptions = cameraOptions;
+
+    if (lastCameraUpdate) {
+        camOptions = {
+            ...cameraOptions,
+            // @ts-ignore
+            position: lastCameraUpdate.position,
+            rotation: lastCameraUpdate.rotation,
+        }
+    }
+
+    return (
+        <div className={style.canvasContainer}>
+            <Canvas
+                ref={ref}
+                data={canvasData}
+                cameraOptions={camOptions}
+                cameraHandler={cameraHandler}
+                backgroundColor={0x505050}
+                onSelection={onSelection}
+                onMount={onMount}
+            />
+        </div>
+    )
+}
+
+export default connect(mapStateToProps)(CanvasExample);
