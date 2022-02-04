7164517fddc5f3488b71b579b40912e809a7acd3
Muhaddatha <mzabdulg@umich.edu>
2022-02-03 23:18:33 -0500

    #246 add description to movie player props

--- geppetto.js/geppetto-ui/src/movie-player/MoviePlayer.js
+++ geppetto.js/geppetto-ui/src/movie-player/MoviePlayer.js
@@ -77,9 +77,10 @@ MoviePlayer.defaultValues = {
     playAtStart: false,
     loop: false,
     volume: 10,
-    width: '800px',
-    height: '640px',
-  }
+    playbackRate: 1,
+  },
+  width: '800px',
+  height: '640px',
 }
 
 MoviePlayer.propTypes = {
@@ -91,9 +92,21 @@ MoviePlayer.propTypes = {
    * Javascript object with playback settings.
    */
   controls: PropTypes.shape({
+    /**
+   * Boolean value indicating if video should play at the start. Defaults to false.
+   */
     playAtStart : PropTypes.bool,
+    /**
+   * Boolean value indicating if the video should be played in a loop. Defaults to false.
+   */
     loop : PropTypes.bool,
+    /**
+   * Start volume of the video.
+   */
     volume : PropTypes.number,
+    /**
+   * Playback rate of the video.
+   */
     playbackRate : PropTypes.number,
   }),
   /**
