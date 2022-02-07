3e6874069a7a9bbac6cda9fbb1d1f98c4fc2f546
afonso <afonsobspinto@gmail.com>
2022-02-07 14:28:54 +0000

    #276 Fix empty case selection manager

--- geppetto.js/geppetto-ui/src/3d-canvas/threeDEngine/SelectionManager.js
+++ geppetto.js/geppetto-ui/src/3d-canvas/threeDEngine/SelectionManager.js
@@ -1,10 +1,15 @@
-
 export const selectionStrategies = Object.freeze(
-  {
-    "nearest": selectedMap => [Object.keys(selectedMap).reduce((selected, current) => selectedMap[current].distance < selectedMap[selected].distance ? current : selected)],
-    "farthest": selectedMap => [Object.keys(selectedMap).reduce((selected, current) => selectedMap[current].distance > selectedMap[selected].distance ? current : selected)],
-    "all": selectedMap => Object.keys(selectedMap)
-  }
+    {
+        "nearest": selectedMap => [Object.keys(selectedMap)
+            .reduce((selected, current) =>
+                selectedMap[current].distance < selectedMap[selected].distance ? current : selected, null)
+        ].filter(s => s),
+        "farthest": selectedMap => [Object.keys(selectedMap)
+            .reduce((selected, current) =>
+                selectedMap[current].distance > selectedMap[selected].distance ? current : selected, null)
+        ].filter(s => s),
+        "all": selectedMap => Object.keys(selectedMap)
+    }
 )
 
   
