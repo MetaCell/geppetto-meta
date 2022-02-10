dcb7fb89b73c3f0a609f8a2408ea549943c24e01
Muhaddatha <mzabdulg@umich.edu>
2022-02-09 20:10:11 -0500

    #95 create tabs for canvas examples

--- geppetto-showcase/src/components/showcase/Showcase.jsx
+++ geppetto-showcase/src/components/showcase/Showcase.jsx
@@ -4,10 +4,13 @@ import Code from './Code';
 import { getConfigFromMarkdown } from './ShowcaseUtils';
 import Paper from '@material-ui/core/Paper';
 import Chip from '@material-ui/core/Chip';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import PropTypes from 'prop-types';
 import PropsTable from './PropsTable';
 import BottomNavigation from '../BottomNavigation';
 
-const styles = (theme) => ({
+const styles = theme => ({
   root: {
     width: '100%',
     paddingBottom: theme.spacing(5),
@@ -46,19 +49,51 @@ const styles = (theme) => ({
     display: 'table',
     margin: '0 auto',
   },
-  library: {
-    margin: theme.spacing(1),
-  },
+  library: { margin: theme.spacing(1), },
 });
 
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box p={3}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.any.isRequired,
+  value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
 class Showcase extends Component {
-  constructor(props) {
+  constructor (props) {
     super(props);
     this.componentRef = React.createRef();
+    this.state = { tabValue: 0, }
   }
 
-  render() {
-    const { classes, markdown, currentPageHandler } = this.props;
+  render () {
+    const { classes, markdown, currentPageHandler, createTabsForExamples } = this.props;
     const configs = getConfigFromMarkdown(markdown);
 
     return (
@@ -69,23 +104,35 @@ class Showcase extends Component {
           <span className={classes.secondaryDescription}>
             {configs.detailedDescription}
           </span>
-          {configs.examples.map((obj) => {
-            const file = obj.file.default.split('\n').join('\n');
-            return (
-              <div key={obj.name}>
-                <h2 className={classes.secondaryTitle}>{obj.name}</h2>
-                <span className={classes.secondaryDescription}>
-                  {obj.description}
-                </span>
-                <Paper variant="outlined">
-                  <div className={classes.centerComponent}>
-                    <obj.component ref={this.componentRef} />
-                  </div>
-                </Paper>
-                <Code file={file} element={configs.reactElement}></Code>
-              </div>
-            );
-          })}
+          {
+            typeof createTabsForExamples !== 'undefined' && createTabsForExamples
+              ? <>
+                <Tabs value={this.state.tabValue} aria-label="showcase-examples-tabs" variant="fullWidth" onChange={(e, newTabValue) => this.setState(() => ({ tabValue: newTabValue }))}>
+                  {configs.examples.map((obj, index) => (
+                    <Tab label={obj.name} key={obj.name} {...a11yProps(index)}/>
+                  ))}
+                </Tabs>
+              </>
+              : <>
+                {configs.examples.map(obj => {
+                  const file = obj.file.default.split('\n').join('\n');
+                  return (
+                    <div key={obj.name}>
+                      <h2 className={classes.secondaryTitle}>{obj.name}</h2>
+                      <span className={classes.secondaryDescription}>
+                        {obj.description}
+                      </span>
+                      <Paper variant="outlined">
+                        <div className={classes.centerComponent}>
+                          <obj.component ref={this.componentRef} />
+                        </div>
+                      </Paper>
+                      <Code file={file} element={configs.reactElement}></Code>
+                    </div>
+                  );
+                })}
+              </>
+          }
           <h2 className={classes.secondaryTitle}>Props</h2>
           <PropsTable propsConfigs={configs.props} />
           <h2 className={classes.secondaryTitle}>Libraries</h2>
