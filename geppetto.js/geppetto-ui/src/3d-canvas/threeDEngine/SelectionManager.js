dceddff5bb70af3380ab46cb833297da4c43dd55
afonso <afonsobspinto@gmail.com>
2022-03-02 15:15:23 +0000

    Fix selectionManager

--- geppetto.js/geppetto-ui/src/3d-canvas/threeDEngine/SelectionManager.js
+++ geppetto.js/geppetto-ui/src/3d-canvas/threeDEngine/SelectionManager.js
@@ -1,15 +1,19 @@
-export const selectionStrategies = Object.freeze(
-    {
-        "nearest": selectedMap => [Object.keys(selectedMap)
-            .reduce((selected, current) =>
-                selectedMap[current].distance < selectedMap[selected].distance ? current : selected, null)
-        ].filter(s => s),
-        "farthest": selectedMap => [Object.keys(selectedMap)
-            .reduce((selected, current) =>
-                selectedMap[current].distance > selectedMap[selected].distance ? current : selected, null)
-        ].filter(s => s),
-        "all": selectedMap => Object.keys(selectedMap)
-    }
-)
+export const selectionStrategies = Object.freeze({
+    "nearest": selectedMap => [Object.keys(selectedMap)
+        .reduce((selected, current) => {
+            if (!selected) {
+                return current
+            } else {
+                return selectedMap[current].distance < selectedMap[selected].distance ? current : selected
+            }
+        }, null)].filter(s => s), "farthest": selectedMap => [Object.keys(selectedMap)
+        .reduce((selected, current) => {
+            if (!selected) {
+                return current
+            } else {
+                return selectedMap[current].distance > selectedMap[selected].distance ? current : selected
+            }
+        }, null)].filter(s => s), "all": selectedMap => Object.keys(selectedMap)
+})
 
   
