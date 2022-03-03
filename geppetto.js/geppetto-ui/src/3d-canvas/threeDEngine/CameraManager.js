60f453c29df1a581efd5d8907eee02e8ec49f65b
afonso <afonsobspinto@gmail.com>
2022-03-03 12:31:41 +0000

    #234 Fix zoom to logic

--- geppetto.js/geppetto-ui/src/3d-canvas/threeDEngine/CameraManager.js
+++ geppetto.js/geppetto-ui/src/3d-canvas/threeDEngine/CameraManager.js
@@ -15,7 +15,7 @@ export default class CameraManager {
     this.camera.direction = new THREE.Vector3(0, 0, 1);
     this.camera.lookAt(this.sceneCenter);
     this.baseZoom = cameraOptions.baseZoom;
-    this.firstLoad = false;
+    this.isFirstRender = true;
   }
 
   update (cameraOptions) {
@@ -29,16 +29,16 @@ export default class CameraManager {
       rotateSpeed
     } = cameraOptions;
 
-    if (reset || (!this.firstLoad && position === undefined)) {
+    if (reset || (this.isFirstRender && position === undefined && zoomTo === undefined)) {
       this.resetCamera(position);
-      if (!this.firstLoad) {
-        this.firstLoad = true;
+      if (this.isFirstRender) {
+        this.isFirstRender = false;
       }
     } else {
-      if (position && !this.firstLoad) {
+      if (position && this.isFirstRender) {
         this.setCameraPosition(position.x, position.y, position.z);
       }
-      if (rotation && !this.firstLoad) {
+      if (rotation && this.isFirstRender) {
         this.setCameraRotation(
           rotation.rx,
           rotation.ry,
@@ -46,20 +46,20 @@ export default class CameraManager {
           rotation.radius
         );
       }
-      if (!this.firstLoad) {
-        this.firstLoad = true;
-      }
-      if (autoRotate) {
-        this.autoRotate(movieFilter);
-      }
-      if (zoomTo && Array.isArray(zoomTo)) {
+      if (zoomTo && Array.isArray(zoomTo) && this.isFirstRender) {
         const instances = zoomTo.map(element => Instances.getInstance(element));
         if (instances.length > 0) {
           this.zoomTo(instances);
         }
       }
+      if (autoRotate) {
+        this.autoRotate(movieFilter);
+      }
       if (rotateSpeed){
         this.engine.controls.rotateSpeed = rotateSpeed
+      }
+      if (this.isFirstRender) {
+        this.isFirstRender = false;
       }
     }
   }
