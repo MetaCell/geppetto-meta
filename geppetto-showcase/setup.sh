6a2c3bd2a4132e2ff7f58981e3fe64c68177343b
enicolasgomez <enicolasgomez@gmail.com>
2022-12-06 10:58:50 -0300

    FIxed dependency issue and changed setup to be more agressive with cache and dependencies

--- geppetto-showcase/setup.sh
+++ geppetto-showcase/setup.sh
@@ -1,15 +1,23 @@
 yarn global add yalc
+yarn cache clean
+rm -rf package-lock.json
+rm -rf yarn.lock
+rm -rf node_modules
+rm -rf dist
 
 cd ../
 app=$(pwd)
 
 cd $app/geppetto.js/geppetto-core
+rm -rf yarn.lock
 yarn && yarn build && yarn publish:yalc
 
 cd $app/geppetto.js/geppetto-ui
+rm -rf yarn.lock
 yarn && yarn build:src && yarn publish:yalc
 
 cd $app/geppetto.js/geppetto-client
+rm -rf yarn.lock
 yarn && yarn build && yarn publish:yalc
 
 cd $app/geppetto-showcase
