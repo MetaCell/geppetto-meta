028a0a4d49c0e9e5ad9ff9a3dce0ddd21ee747a9
afonso <afonsobspinto@gmail.com>
2022-12-28 19:58:40 +0000

    #133 fix: Remove deprecated/unused features from VR Canvas

--- geppetto.js/geppetto-ui/src/vr-canvas/canvas/Canvas.js
+++ geppetto.js/geppetto-ui/src/vr-canvas/canvas/Canvas.js
@@ -1,47 +1,25 @@
 /* eslint-disable no-template-curly-in-string */
 import React, { Component } from 'react';
-import Resources from '@metacell/geppetto-meta-core/Resources';
 import PropTypes from 'prop-types';
-import Instance from '@metacell/geppetto-meta-core/model/Instance';
-import ArrayInstance from '@metacell/geppetto-meta-core/model/ArrayInstance';
-import Type from '@metacell/geppetto-meta-core/model/Type';
-import Variable from '@metacell/geppetto-meta-core/model/Variable';
-import ModelFactory from '@metacell/geppetto-meta-core/ModelFactory';
+import Resources from '@metacell/geppetto-meta-core/Resources';
+import MeshFactory from '@metacell/geppetto-meta-ui/3d-canvas/threeDEngine/MeshFactory';
 import 'aframe';
 import 'aframe-slice9-component';
-import MeshFactory from '@metacell/geppetto-meta-ui/3d-canvas/threeDEngine/MeshFactory';
 import LaserControls from '../LaserControls';
-import Menu from '../menu/Menu';
 import '../aframe/interactable';
 import '../aframe/thumbstick-controls';
 import '../aframe/rig-wasd-controls';
-import { MAIN_MENU } from '../menu/menuStates';
-import {
-  MENU_CLICK,
-  BACK_MENU,
-  VISUAL_GROUPS,
-  RUN_SIMULATION,
-  COLLAPSE_MENU,
-  BRING_CLOSER,
-} from '../Events';
-
-import {
-  getSimulationData,
-  getVoltageColor,
-} from '../utilities/GeppettoSimulation';
-import ColorController from './ColorController';
+import { BRING_CLOSER, } from '../Events';
 import particle from "@metacell/geppetto-meta-ui/3d-canvas/textures/particle.png";
 import { hasVisualType, hasVisualValue } from "../../3d-canvas/threeDEngine/util";
 
 const HOVER_COLOR = { r: 0.67, g: 0.84, b: 0.9 };
 const SELECTED_COLOR = { r: 1, g: 1, b: 0 };
-const SHORTCUTS = {
-  COLLAPSE_MENU: 109,
-  BRING_CLOSER: 99,
-};
+const SHORTCUTS = { BRING_CLOSER: 99, };
 
 function getProxyInstance (i) {
-  return { instancePath: i.getId(), color: { r: 0, g:1, b: 0, a:1 } }
+  const color = i.getId().startsWith('baskets') ? { r: 0, g:0, b: 1, a:1 } : { r: 1, g:0, b: 0, a:1 }
+  return { instancePath: i.getId(), color: color }
 }
 
 function updateInstancesMap (geppettoInstance, instancesMap) {
@@ -86,12 +64,7 @@ class Canvas extends Component {
     super(props);
     this.state = {
       loadedTextures: false,
-      visualGroups: false,
-      currentMenu: MAIN_MENU.id,
-      isMenuVisible: true,
-      simulation: false,
       time: 0,
-      simulationData: null,
       isReady: false
     };
     this.canvasRef = React.createRef();
@@ -100,15 +73,10 @@ class Canvas extends Component {
     this.handleHover = this.handleHover.bind(this);
     this.handleHoverLeave = this.handleHoverLeave.bind(this);
     this.handleClick = this.handleClick.bind(this);
-    this.handleMenuClick = this.handleMenuClick.bind(this);
     this.handleKeyboardPress = this.handleKeyboardPress.bind(this);
-    // TODO: remove this workaround
-    this.activateVisualGroups = this.activateVisualGroups.bind(this);
     this.threeMeshes = {};
     this.selectedMeshes = {};
     this.hoveredMeshes = {};
-    this.menuHistory = [];
-    this.timer = null;
     this.initTextures(this.handleLoadedTextures);
   }
 
@@ -132,7 +100,6 @@ class Canvas extends Component {
       null,
       THREE    
     );
-    this.colorController = new ColorController(this.meshFactory);
 
     this.sceneRef.current.addEventListener('mesh_hover', this.handleHover);
     this.sceneRef.current.addEventListener(
@@ -140,28 +107,8 @@ class Canvas extends Component {
       this.handleHoverLeave
     );
     this.sceneRef.current.addEventListener('mesh_click', this.handleClick);
-    this.sceneRef.current.addEventListener('menu_click', this.handleMenuClick);
     document.addEventListener('keypress', this.handleKeyboardPress);
-    this.sceneRef.current.addEventListener(
-      COLLAPSE_MENU,
-      this.handleKeyboardPress
-    );
-    // TODO: remove this workaround
-    this.sceneRef.current.addEventListener(VISUAL_GROUPS, evt =>
-      this.activateVisualGroups(
-        evt.detail.groups,
-        evt.detail.mode,
-        evt.detail.instances
-      )
-    );
-
-    const { colorMap } = this.props;
-    if (colorMap !== {}) {
-      for (const path in colorMap) {
-        this.setColor(path, colorMap[path]);
-      }
-    }
-
+    
     this.setEntityMeshes();
   }
 
@@ -169,14 +116,13 @@ class Canvas extends Component {
     const { instances } = this.props;
     if (instances !== nextProps.instances) {
       this.meshFactory.start(getInstancesMap(nextProps.instances));
-      this.setState({ visualGroups: false });
     }
     return true;
   }
 
   componentDidUpdate () {
-    const { colorMap, instances } = this.props;
-    const { visualGroups, time, simulationData, simulation, loadedTextures, isReady } = this.state;
+    const { instances } = this.props;
+    const { loadedTextures, isReady } = this.state;
     if (!isReady) {
       if (loadedTextures) {
         this.meshFactory.setParticleTexture(this.particleTexture);
@@ -186,59 +132,12 @@ class Canvas extends Component {
         })
       }
     }
-
-    if (!visualGroups) {
-      if (colorMap !== {}) {
-        for (const path in colorMap) {
-          this.setColor(path, colorMap[path]);
-        }
-      }
-    }
     this.setEntityMeshes();
-    if (simulation) {
-      if (time === Object.keys(simulationData).length - 1) {
-        this.setState({
-          simulation: false,
-          visualGroups: false,
-          simulationData: null,
-          time: 0,
-        });
-        clearInterval(this.timer);
-      }
-    }
 
-  }
-
-  setColor (path, color) {
-    // eslint-disable-next-line no-eval
-    const entity = eval(path);
-    if (entity.hasCapability('VisualCapability')) {
-      if (entity instanceof Instance || entity instanceof ArrayInstance) {
-        this.meshFactory.setColor(path, color);
-
-        if (typeof entity.getChildren === 'function') {
-          const children = entity.getChildren();
-          for (let i = 0; i < children.length; i++) {
-            this.setColor(children[i].getInstancePath(), color);
-          }
-        }
-      } else if (entity instanceof Type || entity instanceof Variable) {
-        /*
-         * fetch all instances for the given type or variable and call hide on each
-         * TODO: Pass ModelFactory to prop?
-         */
-        const instances = ModelFactory.getAllInstancesOf(entity);
-        for (let j = 0; j < instances.length; j++) {
-          this.setColor(instances[j].getInstancePath(), color);
-        }
-      }
-    }
-    return this;
   }
 
   setEntityMeshes () {
     const canvasEntity = this.canvasRef.current;
-
     const sceneMeshes = [];
     const keysThreeMeshes = Object.keys(this.threeMeshes).filter(
       key => this.threeMeshes[key].visible
@@ -362,180 +261,18 @@ class Canvas extends Component {
     }
   }
 
-  handleMenuClick (evt) {
-    const { model } = this.props;
-    const { currentMenu } = this.state;
-    const { event, detail } = evt.detail;
-    if (event === MENU_CLICK) {
-      this.menuHistory.push(currentMenu);
-      this.setState({ currentMenu: detail });
-    } else if (event === BACK_MENU) {
-      const lastMenu = this.menuHistory.pop();
-      this.setState({ currentMenu: lastMenu });
-    } else if (event === VISUAL_GROUPS) {
-      // eslint-disable-next-line no-eval
-      eval(
-        `network_CA1PyramidalCell.CA1_CG[0].getVisualGroups()[${parseInt(
-          detail,
-          10
-        )}].show(true)`
-      );
-    } else if (event === RUN_SIMULATION) {
-      const t0 = performance.now();
-
-      this.colorController.addColorFunction(
-        ModelFactory.instances.getInstance(
-          ModelFactory.getAllPotentialInstancesEndingWith('.v')
-        ),
-        getVoltageColor
-      );
-      const t1 = performance.now();
-      console.log(`Call to addColorFunction took ${t1 - t0} milliseconds.`);
-      this.timer = setInterval(() => {
-        const { time } = this.state;
-        this.setState({ time: time + model.simulation.step });
-      }, 10);
-      this.setState({
-        currentMenu: MAIN_MENU.id,
-        simulation: true,
-        visualGroups: true,
-        simulationData: getSimulationData(model.simulation),
-      });
-    }
-  }
-
-  /**
-   * Activates a visual group
-   * @param visualGroup
-   * @param mode
-   * @param instances
-   */
-  activateVisualGroups (visualGroup, mode, instances) {
-    this.showVisualGroups(visualGroup, mode, instances);
-    this.setState({ visualGroups: true, });
-  }
-
-  /**
-   * Shows a visual group
-   * @param visualGroups
-   * @param mode
-   * @param instances
-   */
-  showVisualGroups (visualGroups, mode, instances) {
-    for (let i = 0; i < instances.length; i++) {
-      const instance = instances[i];
-      const instancePath = instance.getInstancePath();
-      this.merge(instancePath, true);
-      if (mode) {
-        const mergedMesh = this.meshFactory.meshes[instancePath];
-        const map = mergedMesh.mergedMeshesPaths;
-        // no mergedMeshesPaths means object hasn't been merged, single object
-        if (map != undefined || null) {
-          this.meshFactory.splitGroups(instance, visualGroups);
-          this.showVisualGroupsRaw(
-            visualGroups,
-            instance,
-            this.meshFactory.splitMeshes
-          );
-        } else {
-          this.showVisualGroupsRaw(
-            visualGroups,
-            instance,
-            this.meshFactory.meshes
-          );
-        }
-      }
-    }
-  }
-
-  /**
-   *
-   * @param visualGroups
-   * @param instance
-   * @param meshesContainer
-   */
-  showVisualGroupsRaw (visualGroups, instance, meshesContainer) {
-    const instancePath = instance.getInstancePath();
-    for (const g in visualGroups) {
-      // retrieve visual group object
-      const visualGroup = visualGroups[g];
-
-      // get full group name to access group mesh
-      let groupName = g;
-      if (groupName.indexOf(instancePath) <= -1) {
-        groupName = `${instancePath}.${g}`;
-      }
-
-      // get group mesh
-      const groupMesh = meshesContainer[groupName];
-      groupMesh.visible = true;
-      this.meshFactory.setThreeColor(
-        groupMesh.material.color,
-        visualGroup.color
-      );
-    }
-  }
-
-  /**
-   * Merge mesh that was split before
-   *
-   *            aspectPath - Path to aspect that points to mesh
-   * @param instancePath
-   * @param visible
-   */
-  merge (instancePath, visible) {
-    // get mesh from map
-    const mergedMesh = this.meshFactory.meshes[instancePath];
-
-    // if merged mesh is not visible, turn it on and turn split one off
-    if (!mergedMesh.visible) {
-      for (const path in this.meshFactory.splitMeshes) {
-        // retrieve split mesh that is on the scene
-        const splitMesh = this.meshFactory.splitMeshes[path];
-        if (splitMesh) {
-          if (instancePath == splitMesh.instancePath) {
-            splitMesh.visible = false;
-          }
-        }
-      }
-      if (visible) {
-        // add merged mesh to scene and set flag to true
-        mergedMesh.visible = true;
-      }
-    }
-  }
-
   render () {
     const {
       sceneBackground,
-      model,
       id,
       position,
       rotation,
       embedded,
     } = this.props;
-    const {
-      currentMenu,
-      isMenuVisible,
-      simulation,
-      time,
-      simulationData,
-    } = this.state;
+
     const sceneID = `${id}_scene`;
     const cameraID = `${id}_camera`;
     const modelID = `${id}_model`;
-
-    if (simulation) {
-      const simulationTime = Object.keys(simulationData)[time];
-      for (const v of Object.keys(simulationData[simulationTime])) {
-        const variable = v.substring(0, v.lastIndexOf('.'));
-        this.colorController.colorInstance(
-          variable,
-          getVoltageColor,
-          parseFloat(simulationData[simulationTime][v])
-        );
-      }
-    }
 
     return (
       <a-scene
@@ -587,9 +324,6 @@ class Canvas extends Component {
         >
           <a-camera cursor="rayOrigin: mouse" wasd-controls="enabled:false" />
           <LaserControls id={id} />
-          {isMenuVisible ? (
-            <Menu id={id} currentMenu={currentMenu} currentModel={model} />
-          ) : null}
         </a-entity>
 
         <a-plane
@@ -635,7 +369,6 @@ Canvas.defaultProps = {
   handleModelChange: () => {},
 };
 
-// TODO: Add comments, fix instances subprop
 Canvas.propTypes = {
   /**
    * Instances
