c899f13e94d879b5aff7227ca2e200112358be41
emekauja <ujadninth@gmail.com>
2023-01-10 21:24:22 +0100

    added tabButtons extension

--- geppetto.js/geppetto-client/src/common/layout/LayoutManager.tsx
+++ geppetto.js/geppetto-client/src/common/layout/LayoutManager.tsx
@@ -3,41 +3,35 @@ import * as FlexLayout from '@metacell/geppetto-meta-ui/flex-layout/src/index';
 import Actions from '@metacell/geppetto-meta-ui/flex-layout/src/model/Actions';
 import DockLocation from '@metacell/geppetto-meta-ui/flex-layout/src/DockLocation';
 import Model from '@metacell/geppetto-meta-ui/flex-layout/src/model/Model';
-import {
-  WidgetStatus,
-  Widget,
-  ComponentMap,
-  TabsetPosition,
-  IComponentConfig,
-} from './model';
-import { withStyles, createStyles } from '@material-ui/core/styles';
-import WidgetFactory from './WidgetFactory';
-import TabsetIconFactory from './TabsetIconFactory';
-import defaultLayoutConfiguration from './defaultLayout';
-import { widget2Node, isEqual } from './utils';
+import { WidgetStatus, Widget, ComponentMap, TabsetPosition, IComponentConfig } from './model';
+import { withStyles, createStyles } from '@material-ui/core/styles'
+import WidgetFactory from "./WidgetFactory";
+import TabsetIconFactory from "./TabsetIconFactory";
+import defaultLayoutConfiguration from "./defaultLayout";
+import { widget2Node, isEqual } from "./utils";
 import * as GeppettoActions from '../actions';
 
 import {
   layoutActions,
   removeWidgetFromStore,
   updateWidget,
-  setLayout,
-} from './actions';
+  setLayout
+} from "./actions";
 
 import { MINIMIZED_PANEL } from '.';
 
-const styles = theme =>
-  createStyles({
-    container: {
-      flexGrow: 1,
-      display: 'flex',
-      flexDirection: 'row',
-      alignItems: 'stretch',
-      position: 'relative',
-    },
-    spacer: { width: theme.spacing(1) },
-    flexlayout: { flexGrow: 1, position: 'relative' },
-  });
+
+const styles = (theme) => createStyles({
+  container: {
+    flexGrow: 1,
+    display: 'flex',
+    flexDirection: 'row',
+    alignItems: 'stretch',
+    position: 'relative'
+  },
+  spacer: { width: theme.spacing(1) },
+  flexlayout: { flexGrow: 1, position: 'relative' }
+});
 
 let instance: LayoutManager = null;
 
@@ -90,20 +84,13 @@ class LayoutManager {
    * @param {Widget} widgetConfiguration widget to add
    */
   addWidget(widgetConfiguration: Widget) {
-    if (
-      this.getWidget(widgetConfiguration.id) &&
-      this.model.getNodeById(widgetConfiguration.id)
-    ) {
+    if (this.getWidget(widgetConfiguration.id) && this.model.getNodeById(widgetConfiguration.id)) {
       return this.updateWidget(widgetConfiguration);
     }
     const { model } = this;
     let tabset = model.getNodeById(widgetConfiguration.panelName);
     if (tabset === undefined) {
-      this.createTabSet(
-        widgetConfiguration.panelName,
-        widgetConfiguration.defaultPosition,
-        widgetConfiguration.defaultWeight
-      );
+      this.createTabSet(widgetConfiguration.panelName, widgetConfiguration.defaultPosition, widgetConfiguration.defaultWeight);
     }
     this.model.doAction(
       Actions.addNode(
@@ -120,10 +107,9 @@ class LayoutManager {
    *
    * @param panel
    * @param renderValues
+   * @param tabSetButtons
    */
   onRenderTabSet = (panel, renderValues, tabSetButtons) => {
-    console.log(tabSetButtons, panel, 'Tabset');
-
     if (panel.getType() === 'tabset') {
       if (this.enableMinimize) {
         if (panel.getChildren().length > 0) {
@@ -154,19 +140,16 @@ class LayoutManager {
    *
    * @param panel
    * @param renderValues
+   * @param tabButtons
    */
-  onRenderTab = (panel, renderValues) => {
-    if (panel.getType() === 'tabset' && this.enableMinimize) {
-      if (panel.getChildren().length > 0) {
-        renderValues.buttons.push(
-          <div
-            key={panel.getId()}
-            className="fa fa-window-minimize customIconFlexLayout"
-            onClick={() => {
-              this.minimizeWidget(panel.getActiveNode().getId());
-            }}
-          />
-        );
+  onRenderTab = (panel, renderValues, tabButtons) => {
+    if (panel.getType() === 'tab') {
+      if (Array.isArray(tabButtons) && tabButtons.length > 0) {
+        tabButtons.forEach(Button => {
+          renderValues.buttons.push(
+            <Button key={panel.getId()} panel={panel} />
+          );
+        });
       }
     }
   };
@@ -189,7 +172,7 @@ class LayoutManager {
           // iconFactory={layoutManager.iconFactory.bind(this)}
           onAction={action => layoutManager.onAction(action)}
           onRenderTab={(node, renderValues) =>
-            layoutManager.onRenderTab(node, renderValues)
+            layoutManager.onRenderTab(node, renderValues, config?.tabButtons)
           }
           onRenderTabSet={(node, renderValues) => {
             layoutManager.onRenderTabSet(
@@ -197,7 +180,6 @@ class LayoutManager {
               renderValues,
               config?.tabSetButtons
             );
-            console.log('running');
           }}
         />
       </div>
@@ -208,8 +190,7 @@ class LayoutManager {
    * Get the layout component.
    * @memberof Control
    */
-  getComponent = (config?: IComponentConfig) =>
-    withStyles(styles)(this.Component(this, config));
+  getComponent = (config?: IComponentConfig) => withStyles(styles)(this.Component(this, config));
 
   /**
    * Create a new tab set.
@@ -219,7 +200,7 @@ class LayoutManager {
    */
   private createTabSet(tabsetID, position = TabsetPosition.RIGHT, weight = 50) {
     const { model } = this;
-    const rootNode = model.getNodeById('root');
+    const rootNode = model.getNodeById("root");
 
     const tabset = new FlexLayout.TabSetNode(model, { id: tabsetID });
 
@@ -234,6 +215,7 @@ class LayoutManager {
         break;
       case TabsetPosition.BOTTOM:
       case TabsetPosition.TOP: {
+
         tabset._setWeight(80);
         let hrow = new FlexLayout.RowNode(model, {});
         hrow._setWeight(100);
@@ -246,7 +228,7 @@ class LayoutManager {
           }
         });
         if (position === TabsetPosition.BOTTOM) {
-          hrow._addChild(tabset);
+          hrow._addChild(tabset)
         } else {
           hrow._addChild(tabset, 0);
         }
@@ -280,7 +262,7 @@ class LayoutManager {
       try {
         component.importSession(conf);
       } catch (e) {
-        console.error('Error importing session for', widgetId, e);
+        console.error('Error importing session for', widgetId, e)
       }
     } else {
       // The component may not be yet initialized when loading the session
@@ -315,9 +297,9 @@ class LayoutManager {
    * @memberof Control
    * @memberof Control
    */
-  middleware = store => next => action => {
+  middleware = (store) => (next) => (action) => {
     this.store = store;
-    this.widgetFactory.setStore(store);
+    this.widgetFactory.setStore(store)
 
     let nextAction = true;
     let nextSetLayout = true;
@@ -345,7 +327,7 @@ class LayoutManager {
       }
       case layoutActions.ACTIVATE_WIDGET: {
         action.data.status = WidgetStatus.ACTIVE;
-        const widget = this.getWidget(action.data.id);
+        const widget = this.getWidget(action.data.id)
         widget.status = WidgetStatus.ACTIVE;
         this.updateWidget(widget);
         break;
@@ -384,6 +366,7 @@ class LayoutManager {
     if (nextSetLayout) {
       next(setLayout(this.model.toJson()));
     }
+
   };
 
   /**
@@ -422,7 +405,7 @@ class LayoutManager {
    * @private
    */
   private getWidgets(): Widget[] {
-    return Object.values(this.store.getState().widgets);
+    return Object.values(this.store.getState().widgets)
   }
 
   /**
@@ -439,12 +422,7 @@ class LayoutManager {
       case Actions.SET_ACTIVE_TABSET:
         break;
       case Actions.SELECT_TAB:
-        this.store.dispatch(
-          updateWidget({
-            ...this.getWidget(action.data.tabNode),
-            status: WidgetStatus.ACTIVE,
-          })
-        );
+        this.store.dispatch(updateWidget({ ...this.getWidget(action.data.tabNode), status: WidgetStatus.ACTIVE }))
         break;
       case Actions.DELETE_TAB: {
         if (this.getWidget(action.data.node).hideOnClose) {
@@ -453,20 +431,15 @@ class LayoutManager {
           defaultAction = false;
         } else {
           // remove widget from widgets list
-          this.store.dispatch(removeWidgetFromStore(action.data.node));
+          this.store.dispatch(removeWidgetFromStore(action.data.node))
         }
         break;
       }
       case Actions.MAXIMIZE_TOGGLE:
-        // reminder, widgets are not maximised but tabsets are
+	// reminder, widgets are not maximised but tabsets are
         break;
       case Actions.RENAME_TAB:
-        this.store.dispatch(
-          updateWidget({
-            ...this.getWidget(action.data.node),
-            name: action.data.text,
-          })
-        );
+        this.store.dispatch(updateWidget({ ...this.getWidget(action.data.node), name: action.data.text }))
         break;
       case Actions.ADJUST_SPLIT:
         break;
@@ -511,7 +484,7 @@ class LayoutManager {
    */
   private findMaximizedWidget() {
     return this.getWidgets().find(
-      widget => widget && widget.status == WidgetStatus.MAXIMIZED
+      (widget) => widget && widget.status == WidgetStatus.MAXIMIZED
     );
   }
 
@@ -522,7 +495,7 @@ class LayoutManager {
    * @private
    */
   private getWidget(id): Widget {
-    return this.store.getState().widgets[id];
+    return this.store.getState().widgets[id]
   }
 
   /**
@@ -539,7 +512,7 @@ class LayoutManager {
       // find if there exists another widget in the maximized panel that could take its place
       const panelChildren = model.getActiveTabset().getChildren();
       const index = panelChildren.findIndex(
-        child => child.getId() == action.data.node
+        (child) => child.getId() == action.data.node
       );
     }
   }
@@ -558,7 +531,7 @@ class LayoutManager {
     updatedWidget.status = WidgetStatus.MINIMIZED;
     updatedWidget.defaultPanel = updatedWidget.panelName;
     updatedWidget.panelName = MINIMIZED_PANEL;
-    this.store.dispatch(updateWidget(updatedWidget));
+    this.store.dispatch(updateWidget(updatedWidget))
   }
 
   /**
@@ -570,7 +543,7 @@ class LayoutManager {
   private updateWidget(widget: Widget) {
     const { model } = this;
     const previousWidget = this.getWidget(widget.id);
-    const mergedWidget = { ...previousWidget, ...widget };
+    const mergedWidget = { ...previousWidget, ...widget }
     // TODO: what if widget doesn't have a status here?
 
     if (previousWidget.status != mergedWidget.status) {
@@ -584,9 +557,7 @@ class LayoutManager {
     this.widgetFactory.updateWidget(mergedWidget);
 
     if (this.model.getNodeById(widget.id)) {
-      model.doAction(
-        Actions.updateNodeAttributes(mergedWidget.id, widget2Node(mergedWidget))
-      );
+      model.doAction(Actions.updateNodeAttributes(mergedWidget.id, widget2Node(mergedWidget)));
       if (mergedWidget.status == WidgetStatus.ACTIVE) {
         model.doAction(FlexLayout.Actions.selectTab(mergedWidget.id));
       }
@@ -624,11 +595,7 @@ class LayoutManager {
     const panelName = widget.panelName;
     let tabset = model.getNodeById(panelName);
     if (tabset === undefined) {
-      this.createTabSet(
-        panelName,
-        widget.defaultPosition,
-        widget.defaultWeight
-      );
+      this.createTabSet(panelName, widget.defaultPosition, widget.defaultWeight);
     }
     this.moveWidget(widget);
   }
@@ -646,11 +613,7 @@ class LayoutManager {
   }
 }
 
-export function initLayoutManager(
-  model,
-  componentMap: ComponentMap,
-  iconFactory: TabsetIconFactory
-) {
+export function initLayoutManager(model, componentMap: ComponentMap, iconFactory: TabsetIconFactory) {
   instance = new LayoutManager(model, componentMap, iconFactory);
   return instance;
 }
