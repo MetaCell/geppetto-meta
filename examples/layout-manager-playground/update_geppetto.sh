6a4fbb0eb034747f3f04c27264b336d14606043f
Filippo Ledda <filippo@metacell.us>
2023-08-30 04:04:05 +0200

    Chore: fix playground updates

--- examples/layout-manager-playground/update_geppetto.sh
+++ examples/layout-manager-playground/update_geppetto.sh
@@ -2,4 +2,4 @@ cwd=$(pwd)
 cd ../../geppetto.js/geppetto-client
 yarn build:dev  && yarn publish:yalc
 cd $cwd
-npm upgrade @metacell/geppetto-meta-client --legacy-peer-deps
+npm install --legacy-peer-deps
