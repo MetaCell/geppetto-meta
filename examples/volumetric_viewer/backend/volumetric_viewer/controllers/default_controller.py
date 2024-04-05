f62d1fea84df331488abe4773c9895079dfb10a5
Nrault <nicolas.rault97@gmail.com>
2024-04-05 16:13:29 +0200

    NRRD/NIFTI file import first commit
    Fixed requirements on the server side generated by openapi
    Implemented NRRD/NIFTI conversion in default_controller.py (doesn't work because a full file is given, pynrrd ask for a path only)
    Modified App.jsx to add some input fields and import simpleInstances

--- examples/volumetric_viewer/backend/volumetric_viewer/controllers/default_controller.py
+++ examples/volumetric_viewer/backend/volumetric_viewer/controllers/default_controller.py
@@ -1,5 +1,8 @@
 import connexion
 import six
+import nrrd
+import nibabel as nib
+import numpy as np
 
 from volumetric_viewer.models.inline_response200 import InlineResponse200  # noqa: E501
 from volumetric_viewer.models.inline_response2001 import InlineResponse2001  # noqa: E501
@@ -17,7 +20,11 @@ def generate_volume(file=None):  # noqa: E501
 
     :rtype: InlineResponse2001
     """
-    return 'do some magic!'
+    
+    volumetric_data = import_data(file)
+    write_obj(volumetric_data, 'nifti_test.obj', 3, 50)
+    
+    return 'obj generated'
 
 
 def get_volume(volume_name):  # noqa: E501
@@ -31,3 +38,73 @@ def get_volume(volume_name):  # noqa: E501
     :rtype: InlineResponse200
     """
     return 'do some magic!'
+
+def import_data(file):
+	'''import_data:
+		Input:
+		    -filename : Path to the file and name of the file to import (/path/to/the/file/filename)
+		Return:
+		    -data : A 3d array containing volumetric data exported from nifti or nrrd file
+		This function checks wether the input is a nifti or a nrrd file and builds a 3D array out of it
+	'''
+	print(file.filename)
+	if '.nii' in file.filename:
+		print('nifti')
+		nifti_file = nib.load(file.filename)
+		data = nifti_file.get_fdata()
+	if '.nrrd' in file.filename:
+		print('nrrd')
+		data, _ = nrrd.read(file.filename)
+	return data
+    
+def write_obj(volumetric_data, output_obj_path='output.obj', step_size=1, threshold=0):
+	'''write_obj:
+		Input:
+			-volumetric_data : a 3D array extracted from nifti or nrrd file
+			-output_obj_path : the path the OBJ file to create
+			-step_size : the step_size used by the marching cube algorithm, single step can cause performance issues, big steps can decrease the
+			resolution significantly
+			-threshold : threshold to apply to the volumetric data in case of noise
+		This function extract vertices and faces from volumetric data using the marching cube algorithm and write an OBJ file based on that
+	'''
+	volumetric_copy = volumetric_data
+	volumetric_copy[np.where(volumetric_data<=threshold)] = 0
+	vertices, faces, normals, values = measure.marching_cubes(volumetric_copy, 0, step_size=step_size)
+
+	point_str, mesh_str = vertex_face_value_to_str(vertices, faces, values)
+	write_vertices_meshes(point_str, mesh_str, output_obj_path)
+
+
+def vertex_face_value_to_str(vertices, faces, values):    
+	'''vertex_face_value_to_str:
+		Input:
+			-vertices : list of vertices extracted from marching cube algorithm
+			-faces : list of faces extracted from marching cube algorithm
+			-values : volumetric values from the original file
+		Return:
+			-point_str : list of vertices in OBJ format
+			-mesh_str : list of faces in OBJ format
+		This function convert a list of vertices, faces and values into OBJ file format (v1 v2 v3 w/ f1 f2 f3 see OBJ file format)
+	'''
+	point_str = ''
+	mesh_str = ''
+	print('starting writing')
+	for vertex, face, value in zip(vertices, faces, values):
+		point_str = point_str + 'v ' + str(vertex[0]) + ' ' + str(vertex[1]) + ' ' + str(vertex[2]) + ' ' + str(value) + '\n'
+		#face start with index 0, OBJ reads from 1
+		mesh_str = mesh_str + 'f ' + str(face[0]+1) + ' ' + str(face[1]+1) + ' ' + str(face[2]+1) + '\n'
+	return point_str, mesh_str  
+    
+def write_vertices_meshes(vertices, meshes, file):
+	'''write_vertices_meshes:
+		Input:
+			-vertices : a string containing the 3D coordinates of the vertices and their value (v1 v2 v3 w see OBJ file format)
+			-meshes : a string containing the coordinates of the vertices forming each triangle (f1 f2 f3 see OBJ file format)
+			-file : the file in which the shall be written
+		This function writes vertices and meshes into an OBJ file
+	'''
+	with open(file, 'w') as obj_file:
+		obj_file.write(vertices)
+		obj_file.write(meshes)
+	print('done writing')
+
--- examples/volumetric_viewer/frontend/src/App.jsx
+++ examples/volumetric_viewer/frontend/src/App.jsx
@@ -1,7 +1,27 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
+import SimpleInstance from "@metacell/geppetto-meta-core/model/SimpleInstance";
+import Resources from '@metacell/geppetto-meta-core/Resources';
 import './App.css'
+
+const instance1spec = {
+  "eClass": "SimpleInstance",
+  "id": "ANeuron",
+  "name": "The first SimpleInstance to be render with Geppetto Canvas",
+  "type": { "eClass": "SimpleType" },
+  "visualValue": {
+    "eClass": Resources.OBJ,
+    'gltf': null
+  }
+}
+
+function loadInstances (){
+  ModelFactory.cleanModel();
+  const instance1 = new SimpleInstance(instance1spec)
+  window.Instances = [instance1]
+  augmentInstancesArray(window.Instances);
+}
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,9 +38,11 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+      	<input type="text" placeholder="file url"/>
+        <button onClick={() => console.log("conversion!")}>
+          Convert
         </button>
+        <input type="file"/>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
