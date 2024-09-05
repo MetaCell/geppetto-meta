70963b4622de70b297e28f638798c7232ce09a56
aranega <vincent.aranega@gmail.com>
2024-09-05 07:51:26 -0600

    GEP-156 Fix waiting for sub-process to finish in watch script

--- examples/layout-manager-playground/watch-geppettojs-updates.sh
+++ examples/layout-manager-playground/watch-geppettojs-updates.sh
@@ -4,8 +4,14 @@
 PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "${PARENT_PATH}"
 
+function kill_subprocess(){
+    kill $(jobs -p -r)
+}
 
 GEPPETTO_JS="../../geppetto.js"
 (cd "${GEPPETTO_JS}/geppetto-core" && yarn watch) &
 (cd "${GEPPETTO_JS}/geppetto-ui" && yarn watch) &
 (cd "${GEPPETTO_JS}/geppetto-client" && yarn watch) &
+
+trap kill_subprocess INT
+wait
