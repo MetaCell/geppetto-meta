dd852c3031cb028bb9d5ce85efdbdf9374c884b3
aranega <vincent.aranega@gmail.com>
2024-09-09 13:06:16 -0600

    GEP-156 change dependencies order for installation

--- geppetto.js/dev-install.sh
+++ geppetto.js/dev-install.sh
@@ -6,6 +6,6 @@ cd "${PARENT_PATH}"
 
 # Install and init the projects first in reverse order (the most dependent first)
 # each "init" script is linking the projects using yalc
-(cd geppetto-client && yarn install && yarn link:yalc)
-(cd geppetto-ui && yarn install && yarn link:yalc)
 (cd geppetto-core && yarn install && yarn link:yalc)
+(cd geppetto-ui && yarn install && yarn link:yalc)
+(cd geppetto-client && yarn install && yarn link:yalc)
