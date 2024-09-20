35771e8028ce35ee85eb3fae9be73513f1bd9b4e
aranega <vincent.aranega@gmail.com>
2024-09-20 13:58:40 -0600

    GEP-165 Fix issue with model wronlgy serialized

--- examples/load-modify-pygeppetto/backend/pygeppetto_api/api.py
+++ examples/load-modify-pygeppetto/backend/pygeppetto_api/api.py
@@ -13,15 +13,20 @@ from .commands import (
 )
 from pyecore.resources import ResourceSet
 from pyecore.commands import CommandStack
-from pygeppetto.model.model_serializer import serialize as serialize_pygeppetto_model
+from pyecore.resources.json import JsonOptions
+from pygeppetto.model.model import ISynchable
+from pygeppetto.model.model_serializer import IgnoreSyncMapper, SynchableMapper
+from pygeppetto.model.geppetto_resource import GeppettoResource
+from pygeppetto.model.utils.bytesuri import BytesURI
 
 # Stateful APP..
 rset = ResourceSet()
 sessions = {}
 
 # We enable the uuids by default for json
-json_serializer = ResourceSet.resource_factory["json"]
-ResourceSet.resource_factory["json"] = lambda uri: json_serializer(uri, use_uuid=True)
+rset.resource_factory["json"] = lambda uri, *args, **kwargs: GeppettoResource(
+    uri, *args, indent=2, **kwargs
+)
 
 
 def snake2camel(route: APIRoute):
@@ -93,20 +98,39 @@ async def load_model(model_name: str):
     if model_name in sessions:
         (resource, _, _) = sessions[model_name]
         return json.loads(
-            serialize_pygeppetto_model(ultra_shallow_copy(resource.contents[0]))
+            serialize_pygeppetto_model(resource.contents[0], resource_set=rset)
         )
 
     path = Path(__file__).resolve().parent.parent / "models" / f"{model_name}.xmi"
     resource = rset.get_resource(f"{path}", use_uuid=True)
+    root = resource.contents[0]
+    json_model = serialize_pygeppetto_model(root, resource_set=rset)
 
     sessions[model_name] = (
-        resource,
+        root.eResource,
         CommandStack(),
-        CommandRecorder(notifier=resource),
+        CommandRecorder(notifier=root.eResource),
     )
-    return json.loads(
-        serialize_pygeppetto_model(ultra_shallow_copy(resource.contents[0]))
-    )
+    return json.loads(json_model)
 
 
 app.include_router(api_router)
+
+
+# This is a fix version of geppetto_serialize
+def serialize_pygeppetto_model(
+    geppetto_model, onlySerialiseDelta=False, resource_set=None
+):
+    # we now create a resource to save the geppetto model and serialize it to a JSON string
+    rset = resource_set if resource_set else ResourceSet()
+    uri = BytesURI("geppetto_model.json")
+    rset.resource_factory["*"] = lambda uri: GeppettoResource(uri, indent=2)
+    resource = rset.create_resource(uri)
+    resource.append(geppetto_model)
+    if onlySerialiseDelta:
+        resource.register_mapper(ISynchable, SynchableMapper())
+    else:
+        resource.register_mapper(ISynchable, IgnoreSyncMapper())
+
+    resource.save(options={JsonOptions.SERIALIZE_DEFAULT_VALUES: True})
+    return uri.getvalue().decode("utf-8")
