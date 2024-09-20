32c29971b5a5ce74aa9e948b4994cdd593987d4b
aranega <vincent.aranega@gmail.com>
2024-09-19 20:37:05 -0600

    GEP-165 Refactor command creation in the frontend

--- examples/load-modify-pygeppetto/frontend/src/App.tsx
+++ examples/load-modify-pygeppetto/frontend/src/App.tsx
@@ -2,21 +2,8 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { AnyCommand, CommandsService, ModelService, Set, type Compound } from './rest'
-import { add, compound, remove, set } from './commands'
-import { applyPatches, Patch, produceWithPatches } from 'immer'
-
-
-const resolve = (path: string[], model: Record<string, any>) => {
-  let obj = model;
-  for (const sub of path.slice(0, -1)) {
-    obj = obj[sub]
-  }
-  return [obj.uuid, ...path.slice(-1)]
-}
-
-
-
+import { AnyCommand, CommandsService, ModelService } from './rest'
+import { applyModification } from './commands'
 
 function App() {
   const [model, setModel] = useState(null)
@@ -28,53 +15,12 @@ function App() {
   }
 
   const createDummyCommand = async (model: any) => {
-    const [_updated, patch, _inversePath] = produceWithPatches(model, (draft: any) => {
+    const updated = await applyModification(model, (draft: any) => {
       draft.variables[0].name = draft.variables[0].name + 'Bar'
-      draft.variables.push({element: "new"})
-      draft.variables[1].name = 'FOO'
-    });
-
-    console.log("Patch", patch)
-
-    // Translate the command
-    const commands = []
-    for (const p of patch) {
-      if (p.op == "replace") {
-        const [ownerUUID, feature] = resolve(p.path, model)
-        const cmd: Set = set({ owner: {
-          uuid: ownerUUID
-        },
-        feature: feature,
-        value: p.value
-        })
-        commands.push(cmd)
-      }
-      console.log("P", p)
-    }
-    if (commands.length === 0) {
-      console.log("No modification")
-      return
-    }
-    const command = commands.length > 1 ? compound(commands) : commands[0]
-    const notifications = await CommandsService.executeCommand("instances", command)
-
-    // Translate the command back to a immer patch
-    const patches = []
-    for (const notif of notifications) {
-      if (notif.kind === 'set') {
-        patches.push({
-          op: 'replace',
-          path: [...notif.notifier.path?.replace(/[/@]/g, '').split('.'), notif.feature],
-          value: notif.new
-        } as Patch)
-      }
-    }
-
-    const updated = applyPatches(model, patches)
+    })
     setModel(updated)
     setCommands(await CommandsService.listCommands("instances"))
   }
-
 
   return (
     <>
@@ -95,7 +41,7 @@ function App() {
           change variable name
         </button>
         {Object.entries(commands).map(([k, c]) =>
-          <li>{`${k}: ${JSON.stringify(c)}`}</li>
+          <li key={k}>{`${k}: ${JSON.stringify(c)}`}</li>
         )}
         <p>
           {JSON.stringify(model)}
--- examples/load-modify-pygeppetto/frontend/src/commands.ts
+++ examples/load-modify-pygeppetto/frontend/src/commands.ts
@@ -1,4 +1,5 @@
-import { Add, AnyCommand, Compound, Delete, Move, Remove, Set } from "./rest";
+import { applyPatches, Patch, produceWithPatches } from "immer";
+import { Add, AnyCommand, CommandsService, Compound, Delete, Move, Notification, Remove, Set } from "./rest";
 
 
 type SetProps = Omit<Set, 'command_type'>
@@ -36,3 +37,62 @@ export const compound = (commands: Array<AnyCommand>): Compound => {return {
   commands,
   command_type: "compound"
 }}
+
+const resolve = (path: (string | number)[], model: Record<string, any>) => {
+  let obj = model;
+  for (const sub of path.slice(0, -1)) {
+    obj = obj[sub]
+  }
+  return [obj.uuid, ...path.slice(-1)]
+}
+
+export const patch2commands = (patches: Patch[], model: any): (AnyCommand | null) => {
+  const commands = []
+  for (const p of patches) {
+    if (p.op == "replace") {
+      const [ownerUUID, feature] = resolve(p.path, model)
+      const cmd: Set = set({ owner: {
+        uuid: ownerUUID
+      },
+      feature: feature,
+      value: p.value
+      })
+      commands.push(cmd)
+    }
+    // console.log("P", p)
+  }
+  if (commands.length === 0) {
+    return null
+  }
+  return commands.length > 1 ? compound(commands) : commands[0]
+}
+
+export const notification2patches = (notifications: Notification[]): Patch[] => {
+  const patches = []
+  for (const notif of notifications) {
+    if (notif.kind === 'set') {
+      patches.push({
+        op: 'replace',
+        path: [...notif.notifier.path?.replace(/[/@]/g, '').split('.'), notif.feature],
+        value: notif.new
+      } as Patch)
+    }
+  }
+  return patches
+}
+
+export const applyModification = async (model: any, custom: (draft: any) => void) => {
+  const [_updated, _patches, _inversePath] = produceWithPatches(model, custom)
+
+  // Translate the command
+  const command = patch2commands(_patches, model)
+  if (!command) {
+    return null
+  }
+  const notifications = await CommandsService.executeCommand("instances", command)
+
+  // Translate the command back to a immer patch
+  const patches = notification2patches(notifications)
+
+  return applyPatches(model, patches)
+}
