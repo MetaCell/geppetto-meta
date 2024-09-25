dcdc149a8569a2b2c6a4839dcfa38fab6f5e1346
aranega <vincent.aranega@gmail.com>
2024-09-25 11:18:23 -0600

    Fix typo in script names

--- setup.sh
+++ setup.sh
@@ -1,19 +1,14 @@
-yarn global add yalc
+#!/usr/bin/env bash
 
-app=$(pwd)
+# Trick to have folder relative to the script, not CWD
+PARENT_PATH=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
+cd "${PARENT_PATH}"
 
-cd $app/geppetto.js/geppetto-core
-yarn && yarn build && yarn publish:yalc
 
-cd $app/geppetto.js/geppetto-ui
-yarn && yarn build:src && yarn publish:yalc
+# Install the libraries (if not already installed)
+GEPPETTO_JS="../../geppetto.js"
 
-cd $app/geppetto.js/geppetto-client
-yarn && yarn build && yarn publish:yalc
+(bash "${GEPPETTO_JS}/dev-install.bash")
 
-cd $app/geppetto-showcase
-yalc add @metacell/geppetto-meta-client
-yalc add @metacell/geppetto-meta-core
-yalc add @metacell/geppetto-meta-ui
-
-yarn
+# Install the showcase example
+(cd geppetto-showcase && yarn install && yarn link:yalc)
