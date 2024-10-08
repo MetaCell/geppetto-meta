7202aee054dfce716679cdcf6bffe1b97d2f9cf9
aranega <vincent.aranega@gmail.com>
2024-10-08 13:25:51 -0600

    Include plugin to deal with gzip file format with vite

--- examples/vite-redux-toolkit-react-app/vite.config.ts
+++ examples/vite-redux-toolkit-react-app/vite.config.ts
@@ -1,11 +1,30 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const gzipFixPlugin = () => {
+  const fixHeader = (server) => {
+    server.middlewares.use((req, res, next) => {
+      if (req.originalUrl?.includes(".gz")) {
+        res.setHeader("Content-Type", "gzip");
+        res.setHeader("Content-Encoding", "invalid-data");
+      }
+      next();
+    });
+  };
+
+  return {
+    name: "gzip-fix-plugin",
+    configureServer: fixHeader,
+    // vite dev and vite preview use different server, so we need to configure both.
+    configurePreviewServer: fixHeader,
+  };
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), gzipFixPlugin()],
   optimizeDeps: {
     exclude: ['@metacell/geppetto-meta-core', '@metacell/geppetto-meta-client', '@metacell/geppetto-meta-ui']
   },
-  assetsInclude: ['**/*.nii.gz']
+  assetsInclude: ['**/*.nii.gz'],
 })
