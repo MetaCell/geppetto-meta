ba70897beeec4343bc1020a308f09c4a7b8d79c0
Salam Dalloul <salamdalloul93@gmail.com>
2024-10-26 17:00:51 +0300

    add dicom viewer

--- examples/vite-redux-toolkit-react-app/src/layoutManager/componentsMap.tsx
+++ examples/vite-redux-toolkit-react-app/src/layoutManager/componentsMap.tsx
@@ -1,9 +1,9 @@
 import MyComponent from '../components/MyComponent'
-import ThreeDViewer from "../components/viewers/ThreeD/ThreeDViewer.tsx";
+import DicomViewer from "../components/viewers/Dicom/DicomViewer";
 
 const componentMap = {
-  MyComponent,
-  ThreeDViewer,
+  'MyComponent': MyComponent,
+  'Dicom Viewer': DicomViewer
 }
 
 export default componentMap
