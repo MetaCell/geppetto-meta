37eac606eba83a39c70981c2a2477d54320a929a
aranega <vincent.aranega@gmail.com>
2025-09-24 09:28:37 -0600

    GEP-199 Fix issue with box not being interpreted properly

--- examples/vite-3DCanvas-react-app/src/components/viewers/Canvas3DExample.tsx
+++ examples/vite-3DCanvas-react-app/src/components/viewers/Canvas3DExample.tsx
@@ -8,37 +8,43 @@ import * as THREE from "three";
 console.log("three.js for 3D view", THREE.REVISION);
 
 const classes = {
-  container: {
-    height: "100%",
-    width: "100%",
-    display: "flex",
-    alignItems: "stretch",
-  },
+	container: {
+		height: "100%",
+		width: "100%",
+		display: "flex",
+		alignItems: "stretch",
+	},
 };
 
+function MyRotatingBox() {
+	const myMesh = useRef<Mesh>();
+	const [active, setActive] = useState(false);
+
+	useFrame(({ clock }) => {
+		const a = clock.getElapsedTime();
+		myMesh.current.rotation.x = a;
+	});
+
+	return (
+		<mesh
+			scale={active ? 1.5 : 1}
+			onClick={() => setActive(!active)}
+			ref={myMesh}
+		>
+			<boxGeometry />
+			<meshPhongMaterial color="royalblue" />
+		</mesh>
+	);
+}
+
 const Canvas3DExample: React.FC = () => {
-  const myMesh = useRef<Mesh>();
-  const [active, setActive] = useState(false);
-
-  useFrame(({ clock }) => {
-    const a = clock.getElapsedTime();
-    myMesh.current.rotation.x = a;
-  });
-
-  return (
-    <Box className={classes.container} style={classes.container}>
-      <Canvas3D>
-        <mesh
-          scale={active ? 1.5 : 1}
-          onClick={() => setActive(!active)}
-          ref={myMesh}
-        >
-          <boxGeometry />
-          <meshPhongMaterial color="royalblue" />
-        </mesh>
-      </Canvas3D>
-    </Box>
-  );
+	return (
+		<Box className={classes.container} style={classes.container}>
+			<Canvas3D frameloop={"always"}>
+				<MyRotatingBox />
+			</Canvas3D>
+		</Box>
+	);
 };
 
 export default Canvas3DExample;
